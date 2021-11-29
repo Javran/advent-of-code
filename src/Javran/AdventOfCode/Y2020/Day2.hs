4a39b5efaa1d3d134e3d5ab8c7bb6cfb6d206c2d
Javran Cheng <Javran.C@gmail.com>
2021-11-28 23:46:19 -0800

    separate spec and interpretation.

--- src/Javran/AdventOfCode/Y2020/Day2.hs
+++ src/Javran/AdventOfCode/Y2020/Day2.hs
@@ -9,12 +9,19 @@ import Data.Char
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP
 
+type ValidatorSpec = ((Int, Int), Char)
+
 type Validator = String -> Bool
 
 decimal1 :: (Read i, Integral i) => ReadP i
 decimal1 = read <$> munch1 isDigit
 
-validatorP :: ReadP Validator
+buildValidator :: ValidatorSpec -> Validator
+buildValidator ((lo, hi), ch) xs =
+  let cnt = length (filter (== ch) xs)
+   in cnt >= lo && cnt <= hi
+
+validatorP :: ReadP ValidatorSpec
 validatorP = do
   lo <- decimal1
   _ <- char '-'
@@ -22,16 +29,19 @@ validatorP = do
   _ <- char ' '
   c <- get
   _ <- string ": "
-  pure $ \xs ->
-    let cnt = length (filter (== c) xs)
-     in cnt >= lo && cnt <= hi
+  pure ((lo, hi), c)
 
 isValidLine :: String -> Bool
-isValidLine xs = case readP_to_S ((,) <$> validatorP <*> (munch (const True) <* eof)) xs of
-  [((f, v), "")] -> f v
+isValidLine xs = case readP_to_S
+  ((,)
+     <$> validatorP
+     <*> (munch (const True) <* eof))
+  xs of
+  [((fSpec, v), "")] -> buildValidator fSpec v
   _ -> False
 
 main :: IO ()
 main = do
-  getInput @String 2020 2 >>= print . length . filter id . fmap isValidLine . lines
+  rawLines <- lines <$> getInput @String 2020 2
+  print . length . filter id . fmap isValidLine $ rawLines
   pure ()
