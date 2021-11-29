c8ff31c953eeee46fe6b82f7902888eee5fe2551
Javran Cheng <Javran.C@gmail.com>
2021-11-28 21:31:20 -0800

    all day 1 done.

--- src/Javran/AdventOfCode/Y2020/Day1.hs
+++ src/Javran/AdventOfCode/Y2020/Day1.hs
@@ -12,12 +12,26 @@ getInput = do
   xs <- fmap (read @Int) . words <$> readFile "data/2020/day/1/input"
   pure $ IS.fromList xs
 
-solutions :: IS.IntSet -> [] (Int, Int)
+solutions :: IS.IntSet -> [] Int
 solutions xs = do
   x <- IS.toAscList xs
   let (_, ys) = IS.split x xs
-  True <- [IS.member (2020 - x) ys]
-  pure (x, 2020 - x)
+  let y = 2020 - x
+  True <- [IS.member y ys]
+  pure $ x * y
+
+solutions2 :: IS.IntSet -> [] Int
+solutions2 xs = do
+  x <- IS.toAscList xs
+  let (_, ys) = IS.split x xs
+  y <- IS.toAscList ys
+  let (_, zs) = IS.split y ys
+      z = 2020 - x - y
+  True <- [IS.member z zs]
+  pure $ x * y * z
 
 main :: IO ()
-main = getInput >>= print . uncurry (*) . head . solutions
+main = do
+  xs <- getInput
+  print (head (solutions xs))
+  print (head (solutions2 xs))
