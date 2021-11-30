2589ae0992659af38cf501f0d00d6f3a7ff81cac
Javran Cheng <Javran.C@gmail.com>
2021-11-29 20:58:09 -0800

    create a template solution.

---
+++ src/Javran/AdventOfCode/Y2020/DayX.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-typed-holes #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+
+module Javran.AdventOfCode.Y2020.DayX
+  (
+  )
+where
+
+{- HLINT ignore "Unused LANGUAGE pragma" -}
+
+import Control.Monad
+import qualified Data.IntMap.Strict as IM
+import qualified Data.IntSet as IS
+import Data.List
+import qualified Data.List.Split as LSplit
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Javran.AdventOfCode.Prelude
+import Text.ParserCombinators.ReadP
+
+data DayX -- TODO: change to actual number
+
+-- TODO: import to Javran.AdventOfCode.Y2020.Main
+
+instance Solution DayX where
+  solutionIndex _ = (2020, _fixme)
+  solutionRun _ SolutionContext {getInputS, answerShow} = _impl
