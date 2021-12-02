67ce076db496a78fbfbbe4fc5f6877c3b5705155
Javran Cheng <Javran.C@gmail.com>
2021-12-01 18:53:41 -0800

    simplify using divvy.

--- src/Javran/AdventOfCode/Y2021/Day1.hs
+++ src/Javran/AdventOfCode/Y2021/Day1.hs
@@ -7,15 +7,18 @@ module Javran.AdventOfCode.Y2021.Day1
   )
 where
 
-import Data.List
+import Data.List.Split
 import Javran.AdventOfCode.Prelude
 
 data Day1
+
+countIncr :: [Int] -> Int
+countIncr xs = countLength id $ zipWith (<) xs (tail xs)
 
 instance Solution Day1 where
   solutionIndex _ = (2021, 1)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     xs <- fmap (read @Int) . lines <$> getInputS
-    answerShow $ length $ filter id $ zipWith (<) xs (tail xs)
-    let xs' = fmap sum . filter ((== 3) . length) $ fmap (take 3) $ tails xs
-    answerShow $ length $ filter id $ zipWith (<) xs' (tail xs')
+    answerShow $ countIncr xs
+    let xs' = fmap sum $ divvy 3 1 xs
+    answerShow $ countIncr xs'
