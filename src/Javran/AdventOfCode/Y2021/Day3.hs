678f361a6668957106bdecaf878afd070cad7536
Javran Cheng <Javran.C@gmail.com>
2021-12-02 21:57:38 -0800

    cleanup day 3.

--- src/Javran/AdventOfCode/Y2021/Day3.hs
+++ src/Javran/AdventOfCode/Y2021/Day3.hs
@@ -1,73 +1,67 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2021.Day3
   (
   )
 where
 
-{- HLINT ignore "Unused LANGUAGE pragma" -}
-
-import Control.Monad
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
+import Data.Function
 import Data.List
-import Data.List.Split
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP
 
 data Day3
 
-getMostFreq xs = fst $ head $ sortOn (negate . snd) d
-  where
-    d = M.toList $
-      M.fromListWith (+) $ do
-        x <- xs
-        pure (x, 1)
+computeFreq :: Ord a => [a] -> [(a, Int)]
+computeFreq xs = sortOn snd $
+  M.toList $
+    M.fromListWith (+) $ do
+      x <- xs
+      pure (x, 1 :: Int)
 
-computeFreq :: [Bool] -> Bool -> Bool -> Bool
-computeFreq xs tiebreak mostCommon = case d of
+chooseBit :: [Bool] -> Bool -> Bool -> Bool
+chooseBit xs tiebreak mostCommon = case d of
   [(v, _)] -> v
   [(u, x), (v, y)] -> case compare x y of
     EQ -> tiebreak
     LT -> if mostCommon then v else u
     GT -> if mostCommon then u else v
+  _ -> error "Bool can only take 2 values"
   where
     d = M.toList $
       M.fromListWith (+) $ do
         x <- xs
-        pure (x, 1)
+        pure (x, 1 :: Int)
 
-progress oxygen xs i = case transpose xs of
+positionalFilter :: Bool -> [[Bool]] -> Int -> [[Bool]]
+positionalFilter isOxygen xs i = case transpose xs of
   [] -> xs
   ys ->
     let y = ys !! i
-        ft = if oxygen then computeFreq y True True else computeFreq y False False
+        ft =
+          if isOxygen
+            then chooseBit y True True
+            else chooseBit y False False
      in filter (\x -> x !! i == ft) xs
+
+decodeBinary :: (Foldable t, Num a) => t Bool -> a
+decodeBinary = foldl (\acc i -> acc * 2 + if i then 1 else 0) 0
 
 instance Solution Day3 where
   solutionIndex _ = (2021, 3)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     let tr '0' = False
         tr '1' = True
+        tr _ = error "invalid input"
     xs <- (fmap . fmap) tr . lines <$> getInputS
     let ys = transpose xs
-        t = foldl (\acc i -> acc * 2 + if i then 1 :: Int else 0) 0 $ fmap getMostFreq ys
-        t2 = foldl (\acc i -> acc * 2 + if i then 0 else 1) 0 $ fmap getMostFreq ys
-        bin = foldl (\acc i -> acc * 2 + if i then 1 :: Int else 0) 0
-    answerShow $ t * t2
+        getMostFreq = fst . last . computeFreq
+        gamma = fmap getMostFreq ys
+        eps = fmap (not . getMostFreq) ys
+    answerShow $ ((*) `on` decodeBinary @_ @Int) gamma eps
     let l = length (head xs)
-        [oxygenRating] = foldl (\curXs i -> progress True curXs i) xs [0 .. l -1]
-        [co2Rating] = foldl (\curXs i -> progress False curXs i) xs [0 .. l -1]
-    answerShow $ bin oxygenRating * bin co2Rating
+        [oxygenRating] = foldl (\curXs i -> positionalFilter True curXs i) xs [0 .. l -1]
+        [co2Rating] = foldl (\curXs i -> positionalFilter False curXs i) xs [0 .. l -1]
+    answerShow $ ((*) `on` decodeBinary @_ @Int) oxygenRating co2Rating
