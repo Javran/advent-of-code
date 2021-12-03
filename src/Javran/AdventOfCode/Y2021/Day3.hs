e09bfcd8a88e909af22f0c3ae3855a7e4f26497a
Javran Cheng <Javran.C@gmail.com>
2021-12-02 21:35:06 -0800

    quick and dirty day 3.

--- src/Javran/AdventOfCode/Y2021/Day3.hs
+++ src/Javran/AdventOfCode/Y2021/Day3.hs
@@ -29,7 +29,45 @@ import Text.ParserCombinators.ReadP
 
 data Day3
 
+getMostFreq xs = fst $ head $ sortOn (negate . snd) d
+  where
+    d = M.toList $
+      M.fromListWith (+) $ do
+        x <- xs
+        pure (x, 1)
+
+computeFreq :: [Bool] -> Bool -> Bool -> Bool
+computeFreq xs tiebreak mostCommon = case d of
+  [(v, _)] -> v
+  [(u, x), (v, y)] -> case compare x y of
+    EQ -> tiebreak
+    LT -> if mostCommon then v else u
+    GT -> if mostCommon then u else v
+  where
+    d = M.toList $
+      M.fromListWith (+) $ do
+        x <- xs
+        pure (x, 1)
+
+progress oxygen xs i = case transpose xs of
+  [] -> xs
+  ys ->
+    let y = ys !! i
+        ft = if oxygen then computeFreq y True True else computeFreq y False False
+     in filter (\x -> x !! i == ft) xs
+
 instance Solution Day3 where
   solutionIndex _ = (2021, 3)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    getInputS >>= print . length
+    let tr '0' = False
+        tr '1' = True
+    xs <- (fmap . fmap) tr . lines <$> getInputS
+    let ys = transpose xs
+        t = foldl (\acc i -> acc * 2 + if i then 1 :: Int else 0) 0 $ fmap getMostFreq ys
+        t2 = foldl (\acc i -> acc * 2 + if i then 0 else 1) 0 $ fmap getMostFreq ys
+        bin = foldl (\acc i -> acc * 2 + if i then 1 :: Int else 0) 0
+    answerShow $ t * t2
+    let l = length (head xs)
+        [oxygenRating] = foldl (\curXs i -> progress True curXs i) xs [0 .. l -1]
+        [co2Rating] = foldl (\curXs i -> progress False curXs i) xs [0 .. l -1]
+    answerShow $ bin oxygenRating * bin co2Rating
