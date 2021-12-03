835ebc7a29e0f92509b405d3b965c2d290b88294
Javran Cheng <Javran.C@gmail.com>
2021-12-02 19:25:10 -0800

    templating with mustache.

---
+++ src/Javran/AdventOfCode/Cli/New.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+
+module Javran.AdventOfCode.Cli.New
+  ( newCommand
+  )
+where
+
+import Data.Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
+import Javran.AdventOfCode.Cli.Sync (performSync)
+import System.Directory
+import System.Environment
+import System.Exit
+import System.FilePath.Posix
+import Text.Microstache
+
+{-
+  TODO:
+
+  - create module file by applying the template.
+  - perform sync.
+  - create an empty testdata input file.
+
+ -}
+newCommand :: String -> IO ()
+newCommand cmdHelpPrefix =
+  getArgs >>= \case
+    [yearRaw, dayRaw]
+      | [(year, "")] <- reads yearRaw
+        , [(day, "")] <- reads dayRaw -> do
+        projectHome <- getEnv "PROJECT_HOME"
+        let moduleFp =
+              projectHome
+                </> "src"
+                </> "Javran"
+                </> "AdventOfCode"
+                </> ('Y' : show year)
+                </> ("Day" <> show day <> ".hs")
+        -- module file creation.
+        do
+          e <- doesFileExist moduleFp
+          if e
+            then do
+              putStrLn $ "File already exists: " <> moduleFp
+              putStrLn "Skipped module file creation."
+            else do
+              let tmplFp = projectHome </> "data" </> "DayX.hs.mustache"
+                  ctxt =
+                    Object $
+                      HM.fromList
+                        [ ("year", Number $ fromIntegral year)
+                        , ("day", Number $ fromIntegral day)
+                        ]
+
+              tmpl <- compileMustacheFile tmplFp
+              TL.writeFile moduleFp (renderMustache tmpl ctxt)
+              putStrLn $  "Written to: " <> moduleFp
+        -- sync modules
+        performSync
+        print (year :: Int, day :: Int)
+        print moduleFp
+    _ -> die $ cmdHelpPrefix <> "<year> <day>"
--- src/Javran/AdventOfCode/Main.hs
+++ src/Javran/AdventOfCode/Main.hs
@@ -3,7 +3,8 @@ module Javran.AdventOfCode.Main
   )
 where
 
-import qualified Javran.AdventOfCode.Cli.Sync as Sync
+import qualified Javran.AdventOfCode.Cli.New as CliNew
+import qualified Javran.AdventOfCode.Cli.Sync as CliSync
 import Javran.AdventOfCode.Infra
 import qualified Javran.AdventOfCode.Y2020.Main as Y2020
 import qualified Javran.AdventOfCode.Y2021.Main as Y2021
@@ -14,5 +15,6 @@ main =
     "<prog> "
     [ ("2020", Y2020.subMain)
     , ("2021", Y2021.subMain)
-    , ("sync", Sync.syncCommand)
+    , ("sync", CliSync.syncCommand)
+    , ("new", CliNew.newCommand)
     ]
