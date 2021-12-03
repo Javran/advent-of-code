cd5208d5f78a2089ef0937c5997118c7215c6cc2
Javran Cheng <Javran.C@gmail.com>
2021-12-02 22:12:09 -0800

    day 17 done.

--- src/Javran/AdventOfCode/Y2020/Day17.hs
+++ src/Javran/AdventOfCode/Y2020/Day17.hs
@@ -1,38 +1,21 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2020.Day17
-  (
+  ( pprUniv
   )
 where
 
-{- HLINT ignore "Unused LANGUAGE pragma" -}
 
 import Control.Applicative
 import Control.Monad
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
 import Data.List
-import Data.List.Split
 import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Monoid
 import Data.Semigroup
 import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (many)
 
@@ -42,6 +25,8 @@ cellP :: ReadP Bool
 cellP = (False <$ char '.') <++ (True <$ char '#')
 
 type Coord = (Int, Int, Int)
+
+type Coord2 = (Int, Int, Int, Int)
 
 type Universe = S.Set Coord
 
@@ -54,14 +39,24 @@ neighborhoods c@(x, y, z) = do
   guard $ c /= c'
   pure c'
 
-countNeighborhoods :: Universe -> M.Map Coord Int
-countNeighborhoods u = M.fromListWith (+) $ do
+neighborhoods2 :: Coord2 -> [] Coord2
+neighborhoods2 c@(x, y, z, w) = do
+  x' <- [x -1 .. x + 1]
+  y' <- [y -1 .. y + 1]
+  z' <- [z -1 .. z + 1]
+  w' <- [w -1 .. w + 1]
+  let c' = (x', y', z', w')
+  guard $ c /= c'
+  pure c'
+
+countNeighborhoods :: Ord t => (t -> [t]) -> S.Set t -> M.Map t Int
+countNeighborhoods getNeighborhoods u = M.fromListWith (+) $ do
   c <- S.toList u
-  c' <- neighborhoods c
+  c' <- getNeighborhoods c
   pure (c', 1)
 
-stepUniv :: Universe -> Universe
-stepUniv u = S.filter keepAlive u `S.union` newBorns
+stepUniv :: Ord t => (t -> [t]) -> S.Set t -> S.Set t
+stepUniv getNeighborhoods u = S.filter keepAlive u `S.union` newBorns
   where
     newBorns = S.filter comeToLive $ S.difference (M.keysSet nCounts) u
     comeToLive c = case nCounts M.!? c of
@@ -70,7 +65,7 @@ stepUniv u = S.filter keepAlive u `S.union` newBorns
     keepAlive c = case nCounts M.!? c of
       Nothing -> False
       Just cnt -> cnt `elem` [2, 3]
-    nCounts = countNeighborhoods u
+    nCounts = countNeighborhoods getNeighborhoods u
 
 pprUniv :: Universe -> IO ()
 pprUniv u = do
@@ -81,7 +76,11 @@ pprUniv u = do
           ) =
           foldMap
             (\(x, y, z) ->
-               Just ((Min x, Max x), (Min y, Max y), (Min z, Max z)))
+               Just
+                 ( (Min x, Max x)
+                 , (Min y, Max y)
+                 , (Min z, Max z)
+                 ))
             u
   putStrLn $
     intercalate
@@ -94,14 +93,15 @@ pprUniv u = do
   forM_ [minZ .. maxZ] $ \z -> do
     putStrLn $ "z=" <> show z
     forM_ [minY .. maxY] $ \y -> do
-      let tr x = if S.member (x,y,z) u then '#' else '.'
+      let tr x = if S.member (x, y, z) u then '#' else '.'
       putStrLn $ fmap tr [minX .. maxX]
     putStrLn ""
 
 instance Solution Day17 where
   solutionIndex _ = (2020, 17)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    xs <- fmap (fromJust . consumeAllWithReadP (many cellP)) . lines <$> getInputS
+    xs <-
+      fmap (fromJust . consumeAllWithReadP (many cellP)) . lines <$> getInputS
     let initUniv :: Universe
         initUniv = S.fromList $ do
           (r, row) <- zip [0 ..] xs
@@ -109,5 +109,7 @@ instance Solution Day17 where
           guard cell
           -- let x-axis be horizontal and y-axis vertical.
           pure (c, r, 0)
+        initUniv2 = S.map (\(x,y,z) -> (x,y,z,0)) initUniv
 
-    pprUniv (iterate stepUniv initUniv !! 3)
+    answerShow $ S.size (iterate (stepUniv neighborhoods) initUniv !! 6)
+    answerShow $ S.size (iterate (stepUniv neighborhoods2) initUniv2 !! 6)
