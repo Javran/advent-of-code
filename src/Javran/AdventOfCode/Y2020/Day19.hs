57ce5980b4e0726291291eb384ed21d31a612b5b
Javran Cheng <Javran.C@gmail.com>
2021-12-03 18:20:06 -0800

    comment the idea behind this before I forget.

--- src/Javran/AdventOfCode/Y2020/Day19.hs
+++ src/Javran/AdventOfCode/Y2020/Day19.hs
@@ -14,6 +14,7 @@ where
 
 import Control.Applicative
 import Control.Monad
+import Data.Function
 import Data.Function.Memoize (memoFix)
 import qualified Data.IntMap.Strict as IM
 import qualified Data.IntSet as IS
@@ -52,7 +53,13 @@ ruleP =
   where
     tok p = p <* skipSpaces
 
-performMatch rules xs i = case rules IM.!? i of
+type Rules = IM.IntMap Rule
+
+{-
+  performs match, returns unconsumed parts.
+ -}
+performMatch :: Rules -> [Char] -> Int -> Maybe [Char]
+performMatch rules = fix $ \recur xs i -> case rules IM.!? i of
   Nothing -> Nothing
   Just r -> case r of
     RStr ys -> do
@@ -61,48 +68,77 @@ performMatch rules xs i = case rules IM.!? i of
       guard $ xs0 == ys
       pure xs1
     RAlt ys ->
-      let processRSeq ys = foldM (\curXs j -> performMatch rules curXs j) xs ys
-      in foldl1 (<|>) (fmap (\zs -> processRSeq zs) ys)
+      foldl1 (<|>) (fmap (foldM recur xs) ys)
 
+{-
+  we could use S.cartesianProduct here, but probably best to not to:
+  as the resulting elements would be (a,b) that we have to map back to a single one,
+  which rebuilds the whole Set.
+ -}
+setProd :: (Ord a, Monoid a) => S.Set a -> S.Set a -> S.Set a
+setProd sa sb = S.fromList $ do
+  a <- S.toList sa
+  b <- S.toList sb
+  pure (a <> b)
+
+{-
+  For acyclic set of rules, we can compute the complete set of strings
+  that matches the rule.
+ -}
+getMatchingSetOf :: Rules -> Int -> S.Set String
+getMatchingSetOf rules = memoFix $ \query i ->
+  case IM.lookup i rules of
+    Nothing -> S.empty
+    Just rule -> case rule of
+      RStr s -> S.singleton s
+      RAlt ys ->
+        S.unions
+          (fmap (foldl setProd (S.singleton "") . fmap query) ys)
 
 instance Solution Day19 where
   solutionIndex _ = (2020, 19)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    xs <- splitOn [""] . lines <$> getInputS
-    let [rawRules, messages] = xs
-        setProd sa sb = S.fromList $ do
-          {-
-            we could use S.cartesianProduct here, but probably best to not to:
-            as the resulting elements would be (a,b) that we have to map back to a single one,
-            which rebuilds the whole Set.
-           -}
-          a <- S.toList sa
-          b <- S.toList sb
-          pure (a <> b)
-        rulePairP =
-          (,)
-            <$> (decimal1P <* string ": ")
-            <*> ruleP
-        rules :: IM.IntMap Rule
+    [rawRules, messages] <- splitOn [""] . lines <$> getInputS
+    let rulePairP = (,) <$> (decimal1P <* string ": ") <*> ruleP
+        rules :: Rules
         rules =
           IM.fromList $
-            fmap
-              (fromJust
-                 . consumeAllWithReadP rulePairP)
-              rawRules
-        getMatchingSet :: Int -> S.Set String
-        getMatchingSet = memoFix $ \query i ->
-          let processRSeq ys = foldl setProd (S.singleton "") $ fmap query ys
-           in case IM.lookup i rules of
-                Nothing -> S.empty
-                Just rule -> case rule of
-                  RStr s -> S.singleton s
-                  RAlt ys -> S.unions (fmap (\zs -> processRSeq zs) ys)
+            fmap (fromJust . consumeAllWithReadP rulePairP) rawRules
     answerShow (countLength (\msg -> Just "" == performMatch rules msg 0) messages)
+    let getMatchingSet :: Int -> S.Set String
+        getMatchingSet = getMatchingSetOf rules
+
+    {-
+      My key observation:
+
+      Notice the following statements are true for both example input and my specific input:
+
+      - We always have `0: 8 11` as the root rule.
+        In addition, this seems to be the only use site of rule 8 and rule 11.
+      - `8: 42` and `11: 42 31` are present.
+      - all matching strings of rule 8 are of the same length l8
+      - all matching strings of rule 11 are of the same length l11
+
+      Now, by changing:
+
+      - `8: 42` to `8: 42 | 42 8`
+      - `11: 42 31` to `11: 42 31 | 42 11 31`
+
+      we effectively looking for strings that matches the following description:
+
+      > a sequence of {rule 42} repeating m times,
+      > followed by a sequence of {rule 31} repeating n times,
+      > where m > n > 0.
+
+      Now, if we further assume that there is no string that matches both
+      rule 8 and rule 11
+      (if l8 and l11 are different, only match string as prefix on the longer set),
+      we should be able to separate out substring matching rule 42 or rule 31
+      without ambiguity. Then we can verify about m > n > 0, which should identify all matching strings.
+
+     -}
+
     -- TODO: cleanup
-    -- note: `0: 8 11' seems to be the only case that uses 8 and 11.
-    -- 8 repeats 42 at least once
-    -- 11 does 42 31, 42 42 31 31 ... etc. where there is same amount of 42 and 31
     -- rule 42 matches a string of length 8 exactly.
     -- rule 31 matches a string of length 8 exactly.
     {-
