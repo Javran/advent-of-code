04d292c70857c87c3f7e3d72d6d33a1c293804a8
Javran Cheng <Javran.C@gmail.com>
2021-12-05 12:19:56 -0800

    factor this stuff out.

--- src/Javran/AdventOfCode/Y2020/Day25.hs
+++ src/Javran/AdventOfCode/Y2020/Day25.hs
@@ -48,7 +48,7 @@ import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import GHC.Natural
-import GHC.TypeNats (natVal)
+import GHC.TypeNats (KnownNat, natVal)
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (count, many)
 
@@ -56,29 +56,34 @@ data Day25
 
 type ModP = Mod 20201227
 
-subject :: ModP
-subject = 7
-
-rootMod :: Natural
-rootMod = natVal subject
+babyStepGiantStep :: KnownNat n => Mod n -> Mod n -> Maybe Int
+babyStepGiantStep alpha beta = giantStep beta 0
+  where
+    rootMod = natVal alpha
+    {-
+      For computing square root on Int,
+      it seems that we can do no better than a trip to floating points:
+      https://github.com/Bodigrim/integer-roots/blob/7c1bff706149baf071cc41f6415da2a27b492079/Math/NumberTheory/Roots/Squares.hs#L210-L215
+    -}
+    m :: Int
+    m = ceiling (sqrt (fromIntegral rootMod :: Double))
+    babyStep :: HM.HashMap Natural Int
+    babyStep = HM.fromList do
+      j <- [0 .. m -1]
+      pure (unMod (alpha ^% j), j)
+    alphaPowNegM = alpha ^% (- m)
+    giantStep gamma i = do
+      guard $ i < m
+      case HM.lookup (unMod gamma) babyStep of
+        Nothing -> giantStep (gamma * alphaPowNegM) (i + 1)
+        Just j -> pure (i * m + j)
 
 -- https://en.wikipedia.org/wiki/Baby-step_giant-step#The_algorithm
 instance Solution Day25 where
   solutionIndex _ = (2020, 25)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     cPk : dPk : _ <- fmap (fromIntegral @_ @ModP . read @Int) . lines <$> getInputS
-    let m :: Int
-        m = ceiling (sqrt (fromIntegral rootMod :: Double))
-        babyStep :: HM.HashMap Natural Int
-        babyStep = HM.fromList do
-          j <- [0 .. m -1]
-          pure (unMod (subject ^% j), j)
-        aPowNegM = subject ^% (- m)
-        giantStep gamma i = do
-          guard $ i < m
-          case HM.lookup (unMod gamma) babyStep of
-            Nothing -> giantStep (gamma * aPowNegM) (i + 1)
-            Just j -> Just (i * m + j)
-
-    let Just cSk = giantStep cPk 0
+    let subject :: ModP
+        subject = 7
+        Just cSk = babyStepGiantStep subject cPk
     print $ unMod $ dPk ^% cSk
