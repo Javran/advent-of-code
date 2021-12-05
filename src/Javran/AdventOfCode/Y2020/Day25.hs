8a9de711f946eb0f362780d2d19d6eb8da7f32b5
Javran Cheng <Javran.C@gmail.com>
2021-12-05 12:53:07 -0800

    cleanup.

--- src/Javran/AdventOfCode/Y2020/Day25.hs
+++ src/Javran/AdventOfCode/Y2020/Day25.hs
@@ -1,56 +1,19 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2020.Day25
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
-import Data.Bifunctor
-import Data.Bool
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
 import qualified Data.HashMap.Strict as HM
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
-import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
-import Data.Maybe
 import Data.Mod
-import Data.Monoid
-import Data.Proxy
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import GHC.Natural
 import GHC.TypeNats (KnownNat, natVal)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day25
 
@@ -86,4 +49,4 @@ instance Solution Day25 where
     let subject :: ModP
         subject = 7
         Just cSk = babyStepGiantStep subject cPk
-    print $ unMod $ dPk ^% cSk
+    answerShow $ unMod $ dPk ^% cSk
