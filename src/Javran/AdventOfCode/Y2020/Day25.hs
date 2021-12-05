bc1574c398bd09e3ac5b7d07bbe9d2bdba3bf23f
Javran Cheng <Javran.C@gmail.com>
2021-12-05 11:24:51 -0800

    address deprecation.

--- src/Javran/AdventOfCode/Y2020/Day25.hs
+++ src/Javran/AdventOfCode/Y2020/Day25.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
@@ -12,10 +13,10 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-deprecations #-}
 {-# OPTIONS_GHC -Wno-typed-holes #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
 {-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2020.Day25
@@ -38,22 +39,22 @@ import Data.List
 import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
 import Data.Maybe
+import Data.Mod
 import Data.Monoid
 import Data.Semigroup
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (many, count)
-import Math.NumberTheory.Powers.Modular
+import Text.ParserCombinators.ReadP hiding (count, many)
+
 data Day25
 
-rootMod = 20201227
-subject = 7
+type ModP = Mod 20201227
 
 instance Solution Day25 where
   solutionIndex _ = (2020, 25)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    cPk : dPk : _ <- fmap (read @Int) . lines <$> getInputS
-    let (cSk,_) = head [ (i, x) | i <- [1..], let x = powModInt subject i rootMod, x == cPk]
-    print $ powModInt dPk cSk rootMod
+    cPk : dPk : _ <- fmap (fromIntegral @_ @ModP . read @Int) . lines <$> getInputS
+    let (cSk, _) = head [(i, x) | i <- [1 :: Int ..], let x = 7 ^% i, x == cPk]
+    print $ unMod $ dPk ^% cSk
