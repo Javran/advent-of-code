9dec59e982cb216e6e45b40c0d9fda69bf12fdd0
Javran Cheng <Javran.C@gmail.com>
2021-12-04 16:35:19 -0800

    speed up testing by overriding n.
    it seems this is the fastest we can get,
    so let's speed this up by using a smaller n on tests.

--- src/Javran/AdventOfCode/Y2020/Day15.hs
+++ src/Javran/AdventOfCode/Y2020/Day15.hs
@@ -10,14 +10,14 @@ import Control.Monad
 import Control.Monad.ST
 import Data.Function
 import qualified Data.Vector.Unboxed.Mutable as VUM
-import Javran.AdventOfCode.Prelude
 import Data.Word
+import Javran.AdventOfCode.Prelude
 
 data Day15
 
 solve :: [(Word32, Word32)] -> Int -> Word32
 solve xs size = runST $ do
-  vec <- VUM.replicate size 0
+  vec <- VUM.replicate size (0 :: Word32)
   forM_ (init xs) $ \(t, v) ->
     VUM.unsafeWrite vec (fromIntegral v) t
   let (startT, startN) = last xs
@@ -26,10 +26,11 @@ solve xs size = runST $ do
        if turn == fromIntegral size
          then pure val
          else do
-           r <- VUM.unsafeRead vec (fromIntegral val)
-           let num' = if r == 0 then 0 else turn - fromIntegral r
+           let val' = fromIntegral val
+           r <- VUM.unsafeRead vec val'
+           let num' = if r == 0 then 0 else turn - r
                turn' = turn + 1
-           VUM.unsafeWrite vec (fromIntegral val) turn
+           VUM.unsafeWrite vec val' turn
            num' `seq` turn' `seq` loop turn' num')
     startT
     startN
@@ -37,8 +38,13 @@ solve xs size = runST $ do
 instance Solution Day15 where
   solutionIndex _ = (2020, 15)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    [raw] <- lines <$> getInputS
+    (extraOpts, rawInput) <- consumeExtraLeadingLines <$> getInputS
+    let [raw] = lines rawInput
     let xs :: [(Word32, Word32)]
         xs = zip [1 ..] $ fmap read $ splitOn "," raw
     answerShow $ solve xs 2020
-    answerShow $ solve xs 30000000
+    let n = case extraOpts of
+          Nothing -> 30000000
+          Just [overrideN] -> read overrideN
+          _ -> errInvalid
+    answerShow $ solve xs n
