483e7b5498b0283ad3572cbd64bb9dd044a512bc
Javran Cheng <Javran.C@gmail.com>
2021-12-05 10:38:54 -0800

    need to resolve this not-recompiling issue.

--- test/Javran/AdventOfCode/TestDataSpec.hs
+++ test/Javran/AdventOfCode/TestDataSpec.hs
@@ -5,5 +5,16 @@ module Javran.AdventOfCode.TestDataSpec where
 import Javran.AdventOfCode.TestDataTh
 import Test.Hspec
 
+{-
+
+  TODO: current approach has a problem that
+  when testfiles are updates, this is not recompiled,
+  see what can we do about it.
+
+  probably we can do something stupid,
+  like edit this file every time we write-expect.
+
+ -}
+
 spec :: Spec
 spec = mkSpecFromStructuredTestData $collectTests
