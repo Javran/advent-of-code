2dc71bc8e6843070a345ade421a1127100172a79
Javran Cheng <Javran.C@gmail.com>
2021-12-05 12:06:14 -0800

    impl baby-step giant-step.

--- src/Javran/AdventOfCode/Y2020/Day25.hs
+++ src/Javran/AdventOfCode/Y2020/Day25.hs
@@ -33,6 +33,7 @@ import Data.Bool
 import Data.Char
 import Data.Function
 import Data.Function.Memoize (memoFix)
+import qualified Data.HashMap.Strict as HM
 import qualified Data.IntMap.Strict as IM
 import qualified Data.IntSet as IS
 import Data.List
@@ -41,10 +42,13 @@ import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Mod
 import Data.Monoid
+import Data.Proxy
 import Data.Semigroup
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import GHC.Natural
+import GHC.TypeNats (natVal)
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (count, many)
 
@@ -52,9 +56,29 @@ data Day25
 
 type ModP = Mod 20201227
 
+subject :: ModP
+subject = 7
+
+rootMod :: Natural
+rootMod = natVal subject
+
+-- https://en.wikipedia.org/wiki/Baby-step_giant-step#The_algorithm
 instance Solution Day25 where
   solutionIndex _ = (2020, 25)
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     cPk : dPk : _ <- fmap (fromIntegral @_ @ModP . read @Int) . lines <$> getInputS
-    let (cSk, _) = head [(i, x) | i <- [0 :: Int ..], let x = 7 ^% i, x == cPk]
+    let m :: Int
+        m = ceiling (sqrt (fromIntegral rootMod :: Double))
+        babyStep :: HM.HashMap Natural Int
+        babyStep = HM.fromList do
+          j <- [0 .. m -1]
+          pure (unMod (subject ^% j), j)
+        aPowNegM = subject ^% (- m)
+        giantStep gamma i = do
+          guard $ i < m
+          case HM.lookup (unMod gamma) babyStep of
+            Nothing -> giantStep (gamma * aPowNegM) (i + 1)
+            Just j -> Just (i * m + j)
+
+    let Just cSk = giantStep cPk 0
     print $ unMod $ dPk ^% cSk
