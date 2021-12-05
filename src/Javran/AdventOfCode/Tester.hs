33182fb8e12550b0e990771f181acd2faebd4410
Javran Cheng <Javran.C@gmail.com>
2021-12-05 14:27:11 -0800

    a function to digest testdata dir.

--- src/Javran/AdventOfCode/Tester.hs
+++ src/Javran/AdventOfCode/Tester.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -9,19 +11,31 @@ module Javran.AdventOfCode.Tester
   , StructuredTestData
   , scanTestData
   , subCommand
+  , computeTestdataDirDigestTextRep
   )
 where
 
+import qualified Control.Foldl as Foldl
 import Control.Monad
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import Data.List
 import Data.Maybe
+import Data.Text.Encoding
+import Data.Word
+import qualified Filesystem.Path.CurrentOS as FP
 import Javran.AdventOfCode.Infra
 import Javran.AdventOfCode.Prelude
+import Language.Haskell.TH.Syntax
+import Numeric
 import qualified Paths_advent_of_code as StockData
 import System.Directory
+import System.Environment
 import System.FilePath.Posix
 import Text.ParserCombinators.ReadP
-import Language.Haskell.TH.Syntax
+import Turtle.Prelude hiding (sort)
+import Turtle.Shell
 
 data TestDataInfo = TestDataInfo
   { tag :: String
@@ -79,5 +93,39 @@ scanTestData = do
       let days = sort $ mapMaybe (consumeAllWithReadP @Int decimal1P) daysRaw
       forM days $ \day -> (day,) <$> scanForSolution (year, day)
 
+digestTextRep :: BS.ByteString -> String
+digestTextRep = ($ []) . foldr (\i f -> paddedHex i . f) id . BS.unpack
+  where
+    paddedHex :: Word8 -> ShowS
+    paddedHex v =
+      case showHex v [] of
+        [c] -> (\rs -> '0' : c : rs)
+        ys@[_, _] -> (ys <>)
+        _ -> unreachable
+
+computeTestdataDirDigestTextRep :: IO String
+computeTestdataDirDigestTextRep = do
+  {-
+    Don't load from StockData for scanning:
+
+    - could be the case that the binary is ran without involving Cabal,
+      in which case new testdata stucture won't be reflected there.
+
+    - command line expects PROJECT_HOME to present anyway
+
+   -}
+  projectHome <- getEnv "PROJECT_HOME"
+  dirs <- reduce (fmap sort Foldl.list) do
+    let testdataPath = FP.decodeString projectHome FP.</> "data" FP.</> "testdata"
+    -- let it produce relative path, as what is outside of projectHome should never matter.
+    pushd testdataPath
+    fp <- lstree testdataPath
+    st <- stat fp
+    guard $ isRegularFile st
+    pure (either encodeUtf8 encodeUtf8 $ FP.toText fp)
+  let payload = BSL.intercalate (BSL.singleton 0) $ fmap BSL.fromStrict dirs
+      digest = SHA256.hashlazy payload
+  pure $ digestTextRep digest
+
 subCommand :: String -> IO ()
-subCommand _ = scanTestData >>= mapM_ print
+subCommand _ = computeTestdataDirDigestTextRep >>= putStrLn
