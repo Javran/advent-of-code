8954ba1f242942f446d0764802fdaaeb54dfbba3
Javran Cheng <Javran.C@gmail.com>
2021-12-06 22:42:35 -0800

    cleanup.
    I actually don't know what better to do - did some step-based
    searching but in the end the data size is too small to see any
    significance of further optimization.

--- src/Javran/AdventOfCode/Y2021/Day7.hs
+++ src/Javran/AdventOfCode/Y2021/Day7.hs
@@ -1,62 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2021.Day7
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
-import Control.Monad
-import Data.Bifunctor
-import Data.Bool
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
 import Data.List
 import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
 import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day7 deriving (Generic)
 
 instance Solution Day7 where
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     xs <- sort . fmap (read @Int) . splitOn "," . head . lines <$> getInputS
-    let mid = xs !! (length xs `quot` 2)
-    answerShow $ sum $ fmap (\x -> abs (x-mid)) xs
-    let cost x = let y = abs x in (y * (y+1)) `quot` 2
-        computeCost mz = sum (fmap (\x -> cost (x-mz)) xs)
-    answerShow $ minimum $ fmap computeCost [head xs .. last xs]
-
+    let vs = V.fromList xs
+        mid = vs V.! (V.length vs `quot` 2)
+    answerShow $ sum $ fmap (\x -> abs (x - mid)) xs
+    let cost x = let y = abs x in (y * (y + 1)) `quot` 2
+        computeCost mz = sum (fmap (\x -> cost (x - mz)) xs)
+        (lo, hi) = (V.head vs, V.last vs)
+    answerShow $ minimum $ fmap computeCost [lo .. hi]
