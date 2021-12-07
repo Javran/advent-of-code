f08a750b68a4195f70286b49e7dae0b3119a062b
Javran Cheng <Javran.C@gmail.com>
2021-12-06 19:12:07 -0800

    finished new solution routing mechanism.

--- src/Javran/AdventOfCode/Cli/SolutionCommand.hs
+++ src/Javran/AdventOfCode/Cli/SolutionCommand.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Javran.AdventOfCode.Cli.SolutionCommand
-  ( isSolutionCommand
+  ( parse
   , CommandMode (..)
   , subCommand
   )
@@ -19,8 +19,8 @@ where
 
   Parsing:
 
+  - "ls" for listing.
   - any integer value (<year>) commits to this subcommand.
-
   - <year>: list avaliable day solutions for that year
   - <year> <day> <subcommand> <subcommand args>...: this should preserve
     the old behavior.
@@ -28,23 +28,25 @@ where
  -}
 
 import Control.Monad
+import qualified Data.IntMap.Strict as IM
 import Javran.AdventOfCode.Infra
+import Javran.AdventOfCode.MainMaker
 import Javran.AdventOfCode.Solutions
 import System.Environment
 
 data CommandMode
   = ModeList [String]
   | ModeYear Int [String]
-  | ModerYearDay Int Int [String]
+  | ModeYearDay Int Int [String]
 
-isSolutionCommand :: [String] -> Maybe CommandMode
-isSolutionCommand = \case
+parse :: [String] -> Maybe CommandMode
+parse = \case
   "ls" : xs -> pure $ ModeList xs
   yearRaw : xs | Just year <- consumeAllWithReadP decimal1P yearRaw ->
     case xs of
       dayRaw : ys
         | Just day <- consumeAllWithReadP decimal1P dayRaw ->
-          pure $ ModerYearDay year day ys
+          pure $ ModeYearDay year day ys
       ys -> pure $ ModeYear year ys
   _ -> Nothing
 
@@ -52,5 +54,19 @@ subCommand :: SubCmdContext -> CommandMode -> IO ()
 subCommand ctxt mode = do
   let SubCmdContext {cmdHelpPrefix} = ctxt
   case mode of
-    ModeList _ -> mapM_ (print . (\(SomeSolution s) -> solutionIndex s)) allSolutionsSorted
-    _ -> error "todo"
+    ModeList _ ->
+      mapM_ (print . (\(SomeSolution s) -> solutionIndex s)) allSolutionsSorted
+    ModeYear year _ ->
+      case allSolutions IM.!? year of
+        Nothing -> do
+          putStrLn "No solution available, valid years:"
+          print (IM.keys allSolutions)
+        Just yearSols ->
+          mapM_ (print . (\(_, SomeSolution s) -> solutionIndex s)) (IM.toAscList yearSols)
+    ModeYearDay year day xs ->
+      case getSolution year day of
+        Just ss ->
+          withArgs xs $ runSomeSolution ss ctxt
+        Nothing ->
+          -- no solution for this, but we can probably accept a "new" command.
+          putStrLn "TODO"
