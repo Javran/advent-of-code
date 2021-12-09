4eadb08e198199d23ac05643e032a19655ded6a6
Javran Cheng <Javran.C@gmail.com>
2021-12-08 21:28:11 -0800

    day 9.

--- src/Javran/AdventOfCode/Y2021/Day9.hs
+++ src/Javran/AdventOfCode/Y2021/Day9.hs
@@ -28,6 +28,7 @@ where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.State.Strict
 import Data.Bifunctor
 import Data.Bool
 import Data.Char
@@ -47,12 +48,58 @@ import qualified Data.Text as T
 import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
+import Text.ParserCombinators.ReadP hiding (count, get, many)
 
 data Day9 deriving (Generic)
+
+chToInt :: Char -> Int
+chToInt ch = ord ch - ord '0'
+
+type Coord = (Int, Int)
+
+neighbors :: Coord -> [] Coord
+neighbors c =
+  fmap
+    ($ c)
+    [ first succ
+    , first pred
+    , second succ
+    , second pred
+    ]
+
+findBasin :: M.Map Coord Int -> Coord -> State (S.Set Coord) ()
+findBasin heights coord = do
+  let h :: Int
+      h = heights M.! coord
+  visited <- get
+  if S.member coord visited || h == 9
+    then pure ()
+    else do
+      modify (S.insert coord)
+      let coords :: [Coord]
+          coords = filter (\c' -> M.member c' heights) (neighbors coord)
+      forM_ coords $ \coord' -> do
+        let h' = heights M.! coord'
+        when (S.notMember coord' visited && h' > h) $
+          findBasin heights coord'
 
 instance Solution Day9 where
   solutionSolved _ = False
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    xs <- fmap id . lines <$> getInputS
-    mapM_ print xs
+    xs <- fmap (fmap chToInt) . lines <$> getInputS
+    let heights = M.fromList do
+          (r, row) <- zip [0 ..] xs
+          (c, x) <- zip [0 ..] row
+          pure ((r, c), x)
+        lowPoints = do
+          (coord, h) <- M.toList heights
+          let surroundingHs = mapMaybe (\c' -> heights M.!? c') (neighbors coord)
+          guard $ all (> h) surroundingHs
+          pure (coord, h)
+    answerShow $ sum $ fmap (succ . snd) lowPoints
+    answerShow $
+      product $
+        take 3 $
+          reverse $ sort do
+            (c, _) <- lowPoints
+            pure $ S.size $ execState (findBasin heights c) S.empty
