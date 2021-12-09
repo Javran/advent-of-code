df1edb202d92a9517a850e218cf87e61640350cf
Javran Cheng <Javran.C@gmail.com>
2021-12-08 19:37:43 -0800

    use petbox.

--- src/Javran/AdventOfCode/Prelude.hs
+++ src/Javran/AdventOfCode/Prelude.hs
@@ -13,6 +13,7 @@ module Javran.AdventOfCode.Prelude
   , universe
   , -- infrastructures
     module Javran.AdventOfCode.Infra
+  , module Petbox
   )
 where
 
@@ -24,11 +25,12 @@ import Data.Monoid
 import Javran.AdventOfCode.Infra
   ( Solution (..)
   , SolutionContext (..)
-  , consumeOrDie
   , consumeAllWithReadP
   , consumeExtraLeadingLines
+  , consumeOrDie
   , decimal1P
   )
+import Petbox
 
 universe :: (Enum a, Bounded a) => [a]
 universe = [minBound .. maxBound]
@@ -44,22 +46,3 @@ errInvalid = error "invalid input"
 
 unreachable :: a
 unreachable = error "unreachable"
-
--- | non-deterministically picking an element from the given list,
---   separating the selected element and all other remaining elements
---   the list order is preserved
---   e.g. pick [1,2,3] == [(1,[2,3]),(2,[1,3]),(3,[1,2])]
-pick :: [a] -> [(a, [a])]
-pick xs = map splitAux (init $ zip (inits xs) (tails xs))
-  where
-    splitAux (ls, v : rs) = (v, ls ++ rs)
-    splitAux _ = error "cannot split empty list"
-
-{-
-  like "pick", but whenever an element picked,
-  all elements before it will be dropped. This has the effect of only picking
-  elements in order.
- -}
-pickInOrder :: [a] -> [] (a, [a])
-pickInOrder [] = []
-pickInOrder (x : xs) = (x, xs) : pickInOrder xs
--- src/Javran/AdventOfCode/Y2019/Day4.hs
+++ src/Javran/AdventOfCode/Y2019/Day4.hs
@@ -1,61 +1,21 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2019.Day4
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
-import Data.Bifunctor
-import Data.Bool
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
 import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day4 deriving (Generic)
-
-intToDigits :: Integral i => i -> [Int]
-intToDigits x = ($ []) . foldr (.) id $ unfoldr f x
-  where
-    f 0 = Nothing
-    f n = let (q, r) = n `quotRem` 10 in Just ((++ [fromIntegral r]), q)
 
 solve :: Int -> Int -> [Int]
 solve nFrom nTo = do
@@ -71,7 +31,7 @@ solve2 nFrom nTo = do
   [a, b, c, d, e, f] <- pure $ intToDigits n
   guard $ and $ zipWith (<=) [a, b, c, d, e] [b, c, d, e, f]
   guard $
-    or $
+    or
       [ (a == b) && (b /= c)
       , (b == c) && (a /= b) && (c /= d)
       , (c == d) && (b /= c) && (d /= e)
