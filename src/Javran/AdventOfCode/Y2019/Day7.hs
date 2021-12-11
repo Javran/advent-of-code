b4cec8707130d91c5e8d9e185e55e95abf1454fb
Javran Cheng <Javran.C@gmail.com>
2021-12-11 01:45:50 -0800

    cleanup and rename some tests.

--- src/Javran/AdventOfCode/Y2019/Day7.hs
+++ src/Javran/AdventOfCode/Y2019/Day7.hs
@@ -1,56 +1,22 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2019.Day7
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
-import Data.Bifunctor
-import Data.Bool
-import Data.Char
-import Data.Either
 import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
 import Data.List
 import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
 import Javran.AdventOfCode.Y2019.IntCode
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day7 deriving (Generic)
 
@@ -113,16 +79,17 @@ instance Solution Day7 where
                 pure $ k c
               _ -> error "unexpected"
           let pipeline = foldl1 pipeProgram progs
-          Just result <-
-            fix
+          fromJust
+            <$> fix
               (\loopback curProg nextInput lastOut -> do
                  r0 <- curProg
                  case r0 of
-                   NeedInput k0 -> loopback (k0 $ fromJust nextInput) Nothing lastOut
-                   SentOutput o k1 -> loopback k1 (Just o) (Just o)
+                   NeedInput k0 ->
+                     loopback (k0 $ fromJust nextInput) Nothing lastOut
+                   SentOutput o k1 ->
+                     loopback k1 (Just o) (Just o)
                    Done {} -> pure lastOut)
               pipeline
               (Just 0)
               Nothing
-          pure result
         answerShow $ maximum signals
--- test/Javran/AdventOfCode/TestdataSpec.hs
+++ test/Javran/AdventOfCode/TestdataSpec.hs
@@ -16,6 +16,8 @@ to force the build system to recompile (thus re-run TH) this module again.
 1: the TH stuff doesn't care about file content,
   which is actually read by the unit tests.
 
+TODO: it seems changing comment is not sufficient - let's export this as a string.
+
 FORCE_RECOMP_HASH_BEGIN
 35a4305eabb15c54899a88019d4e148b2e02f98bf62494556d016d6a034b74de
 FORCE_RECOMP_HASH_END
