4d7800697cfc568ae35f398d52f938ed30469a15
Javran Cheng <Javran.C@gmail.com>
2021-12-10 23:11:40 -0800

    a bit more cleanup.

--- src/Javran/AdventOfCode/Y2021/Day11.hs
+++ src/Javran/AdventOfCode/Y2021/Day11.hs
@@ -1,54 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2021.Day11
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
 import Data.Bifunctor
-import Data.Bool
 import Data.Char
-import Data.Either
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
 import Data.List
-import Data.List.Split hiding (sepBy)
+import qualified Data.Map.Merge.Strict as M
 import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
 import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day11 deriving (Generic)
 
@@ -67,10 +40,16 @@ adjacents c@(x, y) = do
   guard $ c' /= c
   pure c'
 
+-- keeps expanding the set of flashing octopus until fixpoint.
 flashingSet :: OctoMap -> S.Set Coord -> S.Set Coord
-flashingSet m alreadyFlashing = S.difference (M.keysSet m') alreadyFlashing
+flashingSet m alreadyFlashing = M.keysSet m'
   where
-    m' = M.filterWithKey (\coord v -> v + (fromMaybe 0 (adjFlashes M.!? coord)) > 9) m
+    m' =
+      M.filterWithKey
+        (\coord v ->
+           let flashing = v + fromMaybe 0 (adjFlashes M.!? coord) > 9
+            in flashing)
+        m
     adjFlashes = M.fromListWith (+) do
       coord <- S.toList alreadyFlashing
       coord' <- adjacents coord
@@ -78,29 +57,29 @@ flashingSet m alreadyFlashing = S.difference (M.keysSet m') alreadyFlashing
 
 flashAux :: OctoMap -> S.Set Coord -> OctoMap
 flashAux m s =
-  if S.null newlyFlashing
+  if S.size s == S.size s'
     then
       let adjFlashes = M.fromListWith (+) do
             coord <- S.toList s
             coord' <- adjacents coord
             pure (coord', 1 :: Int)
-       in M.mapWithKey (\coord v -> v + (fromMaybe 0 (adjFlashes M.!? coord))) m
-    else flashAux m (S.union newlyFlashing s)
+       in M.merge
+            M.preserveMissing
+            M.dropMissing
+            (M.zipWithMatched (const (+)))
+            m
+            adjFlashes
+    else flashAux m s'
   where
-    newlyFlashing = flashingSet m s
+    s' = flashingSet m s
 
 flash :: OctoMap -> (S.Set Coord, OctoMap)
 flash m = (M.keysSet $ M.filter (\v -> v > 9) m', M.map (\v -> if v > 9 then 0 else v) m')
   where
     m' = flashAux m S.empty
 
-incrAllBy1 :: OctoMap -> OctoMap
-incrAllBy1 = M.map succ
-
 step :: OctoMap -> (Int, OctoMap)
-step m = (S.size s', m')
-  where
-    (s', m') = flash . incrAllBy1 $ m
+step = first S.size . flash . M.map succ
 
 instance Solution Day11 where
   solutionRun _ SolutionContext {getInputS, answerShow} = do
@@ -117,6 +96,6 @@ instance Solution Day11 where
                 in Just (w, m'))
             m
     answerShow $ sum $ fmap fst $ take 100 progression
-    let (ans :: Int , _) : _ =
+    let (ans :: Int, _) : _ =
           dropWhile (not . all (== 0) . snd . snd) $ zip [1 ..] progression
     answerShow ans
