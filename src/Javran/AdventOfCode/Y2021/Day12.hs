7490ff7f19eb19a1f7d4f03facb75aa04395a900
Javran Cheng <Javran.C@gmail.com>
2021-12-11 21:57:16 -0800

    cleanup.

--- src/Javran/AdventOfCode/Y2021/Day12.hs
+++ src/Javran/AdventOfCode/Y2021/Day12.hs
@@ -1,93 +1,70 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2021.Day12
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
-import Data.Bifunctor
-import Data.Bool
 import Data.Char
-import Data.Either
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
 import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
 import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import Debug.Trace
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day12 deriving (Generic)
 
 isLarge :: String -> Bool
 isLarge = any isUpper
 
-search graph cur visitedSmalls acc
-  | cur == "end" = pure (reverse $ cur : acc)
-  | otherwise = do
-    next <- S.toList (fromMaybe S.empty (graph M.!? cur))
-    let large = isLarge next
-    guard $ large || S.notMember next visitedSmalls
-    search graph next (if not (isLarge cur) then S.insert cur visitedSmalls else visitedSmalls) (cur : acc)
+type Node = String
 
-search2 graph cur visitedSmalls twiceBudget acc
-  | cur == "end" = pure (reverse $ cur : acc)
+type Graph = M.Map Node (S.Set Node)
+
+type Path = [Node]
+
+findPaths :: Graph -> Node -> S.Set Node -> Bool -> [Node] -> [Path]
+findPaths graph cur visitedSmalls twiceBudget path
+  | cur == "end" = pure (reverse $ cur : path)
   | otherwise = do
-    next <- S.toList (fromMaybe S.empty (graph M.!? cur))
+    Just nextsSet <- pure $ graph M.!? cur
+    next <- S.toList nextsSet
     guard $ next /= "start"
-    let large = isLarge next
-        alreadyVisited = S.member next visitedSmalls
-    guard $ large || (not alreadyVisited || twiceBudget)
-    let visitedSmalls' = if not (isLarge cur) then S.insert cur visitedSmalls else visitedSmalls
-    search2
+    let nextAlreadyVisited = S.member next visitedSmalls
+    guard $
+      isLarge next
+        || (-- next is a small cave
+            not nextAlreadyVisited || twiceBudget)
+    let visitedSmalls' =
+          if isLarge cur
+            then visitedSmalls
+            else S.insert cur visitedSmalls
+        twiceBudget'
+          | isLarge next =
+            -- not using it.
+            twiceBudget
+          | nextAlreadyVisited =
+            -- using budget on this one
+            False
+          | otherwise =
+            -- carry over.
+            twiceBudget
+    findPaths
       graph
       next
       visitedSmalls'
-      (if large
-         then twiceBudget
-         else
-           (if alreadyVisited then False else twiceBudget))
-      (cur : acc)
+      twiceBudget'
+      (cur : path)
 
 instance Solution Day12 where
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     xs <- fmap ((\[a, b] -> (a, b)) . splitOn "-") . lines <$> getInputS
-    let graph = M.fromListWith (<>) do
+    let graph :: Graph
+        graph = M.fromListWith (<>) do
           (u, v) <- xs
           [(u, S.singleton v), (v, S.singleton u)]
-    answerShow $ length $ (search2 graph "start" (S.singleton "start") False [])
-    answerShow $ length (search2 graph "start" (S.singleton "start") True [])
+    forM_ [False, True] $ \twiceBudget -> do
+      answerShow $ length $ findPaths graph "start" (S.singleton "start") twiceBudget []
