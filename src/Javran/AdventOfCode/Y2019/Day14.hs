5a454f78def3f9fea927e8fc812ba76725f7b970
Javran Cheng <Javran.C@gmail.com>
2021-12-14 00:54:19 -0800

    day 14 cleanup.

--- src/Javran/AdventOfCode/Y2019/Day14.hs
+++ src/Javran/AdventOfCode/Y2019/Day14.hs
@@ -1,53 +1,19 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2019.Day14
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Writer.CPS
-import Data.Bifunctor
-import Data.Bool
 import Data.Char
-import Data.Either
 import Data.Foldable
 import Data.Function
-import Data.Function.Memoize (memoFix)
 import qualified Data.Graph as G
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
-import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (count, many)
@@ -67,7 +33,7 @@ reactionP = do
   y <- amountChemP
   pure (M.fromList xs, y)
   where
-    amountChemP = (flip (,)) <$> decimal1P <*> (char ' ' *> munch1 isAlpha)
+    amountChemP = flip (,) <$> decimal1P <*> (char ' ' *> munch1 isAlpha)
 
 type ReactionTable = M.Map Chem (InputChems, Int)
 
@@ -96,7 +62,7 @@ solveFuel :: M.Map Chem (InputChems, Int) -> [Chem] -> Int -> Int
 solveFuel reactions elimOrder n = answer
   where
     initNeeds = M.singleton "FUEL" n
-    m = foldl (\demand c -> (elim reactions c demand)) initNeeds elimOrder
+    m = foldl (\demand c -> elim reactions c demand) initNeeds elimOrder
     [("ORE", answer)] = M.toList m
 
 instance Solution Day14 where
