5d7c366957f03b084f702104b56ebe67a4b95246
Javran Cheng <javran@google.com>
2021-12-21 14:22:08 -0800

    correction on how cut should be performed.
    using 0,1,2,3... as an example is a bad idea.

--- src/Javran/AdventOfCode/Y2019/Day22.hs
+++ src/Javran/AdventOfCode/Y2019/Day22.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -39,6 +40,7 @@ import Data.List
 import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
 import Data.Maybe
+import Data.Mod
 import Data.Monoid
 import Data.Ord
 import Data.Semigroup
@@ -47,6 +49,7 @@ import qualified Data.Text as T
 import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
+import Math.NumberTheory.Moduli.Class
 import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day22 deriving (Generic)
@@ -57,11 +60,18 @@ data Day22 deriving (Generic)
 
   - deal into new stack:
 
-   x => p - x, it's revert is: x => p - x (same)
+   x => - x, its revert is: x => - x (same)
 
   - cut N:
 
-   x => x + N, it's revert is: x => x - N
+   This one is actually the tricky one:
+
+   + find 0-th item i and N-th item j in current sequence
+
+   + x => x + (j - i), this shifts the N-th item to 0-th location,
+     and the rest of the items just follow.
+
+   To revert this, simply do `cut -N`.
 
   - deal with increment N:
 
@@ -71,8 +81,50 @@ data Day22 deriving (Generic)
 
  -}
 
+data ShufTech
+  = StDealIntoNewStack
+  | StCutCards Int
+  | StDealWithIncrement Int
+  deriving (Show)
+
+shufTechP :: ReadP ShufTech
+shufTechP =
+  (StCutCards <$> (string "cut " *> readS_to_P (reads @Int)))
+    <++ do
+      _ <- string "deal "
+      (StDealIntoNewStack <$ string "into new stack")
+        <++ (StDealWithIncrement <$> (string "with increment " *> decimal1P))
+
+revertShufTech :: SomeMod -> ShufTech -> SomeMod
+revertShufTech x = \case
+  StDealIntoNewStack -> x * (-1)
+  StCutCards n -> x - fromIntegral n -- TODO: definitely wrong.
+  StDealWithIncrement n -> x * fromIntegral n
+
+applyShufTech :: SomeMod -> ShufTech -> SomeMod
+applyShufTech x = \case
+  StDealIntoNewStack -> x * (-1)
+  StCutCards n -> x + fromIntegral n -- TODO: definitely wrong.
+  StDealWithIncrement n -> case x of
+    SomeMod x' -> SomeMod (x' * fromJust (invertMod (fromIntegral n `asTypeOf` x')))
+    _ -> unreachable
+
 instance Solution Day22 where
   solutionSolved _ = False
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    xs <- fmap id . lines <$> getInputS
-    mapM_ print xs
+    xs <- fmap (consumeOrDie shufTechP) . lines <$> getInputS
+    let extract = \case
+          SomeMod m -> getVal m
+          _ -> unreachable
+    print (length xs)
+    print
+      (fmap
+         (\k ->
+            extract $
+              foldl
+                applyShufTech
+                (k `modulo` 10)
+                [ StDealWithIncrement 7
+                , StDealWithIncrement 9
+                ])
+         [0 .. 9])
