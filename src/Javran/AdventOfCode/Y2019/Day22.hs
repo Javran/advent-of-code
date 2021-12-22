d2f2fb6cb50641c08deb63516d9a25e6dba98893
Javran Cheng <Javran.C@gmail.com>
2021-12-21 18:28:46 -0800

    nuke it.
    fuck this shit. nothing works, heading for the stupid route first.

--- src/Javran/AdventOfCode/Y2019/Day22.hs
+++ src/Javran/AdventOfCode/Y2019/Day22.hs
@@ -54,33 +54,6 @@ import Text.ParserCombinators.ReadP hiding (count, many)
 
 data Day22 deriving (Generic)
 
-{-
-
-  Note: all revertible under modulo p:
-
-  - deal into new stack:
-
-   x => - x, its revert is: x => - x (same)
-
-  - cut N:
-
-   This one is actually the tricky one:
-
-   + find 0-th item i and N-th item j in current sequence
-
-   + x => x + (j - i), this shifts the N-th item to 0-th location,
-     and the rest of the items just follow.
-
-   To revert this, simply do `cut -N`.
-
-  - deal with increment N:
-
-   x => x * N^-1, it's revert is x => x * N
-
-   where N^-1 * N = 1
-
- -}
-
 data ShufTech
   = StDealIntoNewStack
   | StCutCards Int
@@ -95,36 +68,8 @@ shufTechP =
       (StDealIntoNewStack <$ string "into new stack")
         <++ (StDealWithIncrement <$> (string "with increment " *> decimal1P))
 
-revertShufTech :: SomeMod -> ShufTech -> SomeMod
-revertShufTech x = \case
-  StDealIntoNewStack -> x * (-1)
-  StCutCards n -> x - fromIntegral n -- TODO: definitely wrong.
-  StDealWithIncrement n -> x * fromIntegral n
-
-applyShufTech :: SomeMod -> ShufTech -> SomeMod
-applyShufTech x = \case
-  StDealIntoNewStack -> x * (-1)
-  StCutCards n -> x + fromIntegral n -- TODO: definitely wrong.
-  StDealWithIncrement n -> case x of
-    SomeMod x' -> SomeMod (x' * fromJust (invertMod (fromIntegral n `asTypeOf` x')))
-    _ -> unreachable
-
 instance Solution Day22 where
   solutionSolved _ = False
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     xs <- fmap (consumeOrDie shufTechP) . lines <$> getInputS
-    let extract = \case
-          SomeMod m -> getVal m
-          _ -> unreachable
     print (length xs)
-    print
-      (fmap
-         (\k ->
-            extract $
-              foldl
-                applyShufTech
-                (k `modulo` 10)
-                [ StDealWithIncrement 7
-                , StDealWithIncrement 9
-                ])
-         [0 .. 9])
