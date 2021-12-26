6d2fafb091b63630748dd9e036002833c753a6d9
Javran Cheng <Javran.C@gmail.com>
2021-12-25 16:58:34 -0800

    improve cracking method.

--- src/Javran/AdventOfCode/Y2019/Day25/Cracker.hs
+++ src/Javran/AdventOfCode/Y2019/Day25/Cracker.hs
@@ -56,48 +56,78 @@ import Javran.AdventOfCode.Y2019.Day25.ResponseParser
 import Javran.AdventOfCode.Y2019.IntCode
 import Text.ParserCombinators.ReadP hiding (count, get, many, manyTill)
 
-{-
-  TODO: this is current just a dump cracker that tries everything.
- -}
+type Cracker = StateT CrackerState IO
+
+data CrackerState = CrackerState
+  { csSearchSpace :: S.Set (S.Set String) -- current search space
+  , csInventory :: S.Set String -- current inventory
+  , csProg :: IO AsciiResult
+  }
+
+takeOrDropItem :: Bool -> String -> Cracker ()
+takeOrDropItem isTake item = do
+  p <- gets csProg
+  r0 <- liftIO p
+  case r0 of
+    AsciiNeedCommand k -> do
+      AsciiOutput _ k' <- liftIO $ k $ (if isTake then "take " else "drop ") <> item
+      modify \cs -> cs {csProg = k'}
+    _ -> error "unexpected"
+
+crack :: Dir -> Cracker Int
+crack psfDir = do
+  ss <- gets csSearchSpace
+  -- liftIO $ putStrLn $ "Search space size: " <> show (S.size ss)
+  when (null ss) $
+    error "search space exhausted"
+  let tryInv = S.toList ss !! (S.size ss `quot` 2)
+  -- TODO: this can be smarter
+  curInv <- gets csInventory
+  forM_ (S.toList curInv) \item ->
+    takeOrDropItem False item
+  forM_ (S.toList tryInv) \item ->
+    takeOrDropItem True item
+  modify \cs -> cs {csInventory = tryInv}
+  p0 <- gets csProg
+  AsciiNeedCommand k0 <- liftIO p0
+  AsciiOutput out k1 <- liftIO $ k0 (fmap toLower $ show psfDir)
+  modify \cs -> cs {csProg = k1}
+  case consumeAllWithReadP responsesP out of
+    Just
+      [ RespRoomInfo
+          RoomInfo
+            { riPressureSensitiveExtra =
+              Just (ParFailure {parFailureShouldBeLighter})
+            }
+        , _
+        ] -> do
+        {-
+          when parFailureShouldBeLighter is True:
+            the expected droid weight is lighter than we are,
+            meaning everything containing current inventory cannot be the solution.
+         -}
+        let shouldExclude =
+              if parFailureShouldBeLighter
+                then (tryInv `S.isSubsetOf`)
+                else (`S.isSubsetOf` tryInv)
+        modify \cs -> cs {csSearchSpace = S.filter (not . shouldExclude) $ csSearchSpace cs}
+        crack psfDir
+    Just
+      [ RespRoomInfo
+          RoomInfo
+            { riPressureSensitiveExtra =
+              Just (ParSuccess ans)
+            }
+        ] -> pure ans
+    xs -> error $ "unexpected response: " <> show xs
+
 runCracker :: ExplorerResult -> IO Int
 runCracker (fullInventory, psfDir, prog) = do
-  let allCombinations = filterM (const [False, True]) fullInventory
-      dropEverything = ["drop " <> x | x <- fullInventory]
-      tryComb :: [String] -> [String]
-      tryComb items =
-        ["take " <> item | item <- items] <> [fmap toLower $ show psfDir]
-          <> ["drop " <> item | item <- items]
-      tryAllCombinations = do
-        comb <- allCombinations
-        tryComb comb
-      presetCommands = dropEverything <> tryAllCombinations
-  fix
-    (\loop curP curPresetCmds -> do
-       result <- curP
-       case result of
-         AsciiDone -> error "preset command exhausted."
-         AsciiNeedCommand k -> do
-           (cmd, nextPresetCmds) <- case curPresetCmds of
-             [] ->
-               error "preset command exhausted."
-             (pCmd : cs') -> do
-               pure (pCmd, cs')
-           loop (k cmd) nextPresetCmds
-         AsciiOutput outs k -> do
-           let r = debugConsumeAllWithReadP responsesP outs
-               cont = loop k curPresetCmds
-           case r of
-             Right
-               [ RespRoomInfo
-                   RoomInfo
-                     { riPressureSensitiveExtra = Just (ParSuccess v)
-                     }
-                 ] -> pure v
-             Right _ ->
-               cont
-             Left msg -> do
-               putStrLn $ "warning: parse failure, reason: " <> msg
-               putStr outs
-               cont)
-    prog
-    presetCommands
+  let fullInv = S.fromList fullInventory
+  evalStateT
+    (crack psfDir)
+    CrackerState
+      { csSearchSpace = S.powerSet fullInv
+      , csInventory = fullInv
+      , csProg = prog
+      }
