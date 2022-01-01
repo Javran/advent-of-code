8cba70d284ce737398d80eff6a99d0fc6ee688cd
Javran Cheng <Javran.C@gmail.com>
2022-01-01 12:54:02 -0800

    cure some Bool-blindness and cleanup.

--- src/Javran/AdventOfCode/Y2018/Day13.hs
+++ src/Javran/AdventOfCode/Y2018/Day13.hs
@@ -1,53 +1,31 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2018.Day13
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
 import Control.Monad.State.Strict
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
 import Data.List
-import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
-import Data.Monoid
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
 import Data.Void
-import Debug.Trace
-import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Text.ParserCombinators.ReadP hiding (count, get, many)
 
 data Day13 deriving (Generic)
+
+{-
+  coord system:
+  o ----- > x
+  |
+  |
+  V
+  y
+
+ -}
 
 data Dir = U | L | D | R deriving (Show, Ord, Eq, Enum)
 
@@ -62,16 +40,6 @@ turnLeft, turnRight :: Dir -> Dir
 (turnLeft, turnRight) = (\d -> ds !! (fromEnum d + 1), \d -> ds !! (fromEnum d + 3))
   where
     ds = cycle [U .. R]
-
-{-
-  coord system:
-  o ----- > x
-  |
-  |
-  V
-  y
-
- -}
 
 type Coord = (Int, Int) -- x and y
 
@@ -95,6 +63,94 @@ data CartState = CartState
   , csTurnOpt :: [Dir -> Dir] -- infinite list of turning decisions, to be used on intersections
   }
 
+parseSym :: Char -> Maybe Sym
+parseSym = \case
+  ' ' -> Nothing
+  '\\' -> Just $ SCurve False
+  '/' -> Just $ SCurve True
+  '+' -> Just SIntersect
+  '^' -> Just $ SCart U
+  '<' -> Just $ SCart L
+  'v' -> Just $ SCart D
+  '>' -> Just $ SCart R
+  '-' -> Just $ SPipe False
+  '|' -> Just $ SPipe True
+  s -> error $ "Unknown: " <> show s
+
+parseFromRaw :: [String] -> MapInfo
+parseFromRaw raw = MapInfo {miGraph, miCurves, miCarts}
+  where
+    miCurves = M.fromList do
+      (coord, SCurve v) <- M.toList syms
+      pure (coord, v)
+    miCarts = do
+      (coord, SCart d) <- M.toList syms
+      pure (coord, d)
+    miGraph = M.fromList do
+      (coord, sym) <- M.toList syms
+      let -- checks connectivity from current coord to a neighborhood.
+          conn :: Dir -> Maybe Coord
+          conn dir = do
+            let coord' = applyDir dir coord
+                expectVertical = dir `elem` [U, D]
+                tt = Just coord'
+            s' <- syms M.!? coord'
+            case s' of
+              SCurve _ -> tt
+              SIntersect -> tt
+              SCart d' -> do
+                guard
+                  if expectVertical
+                    then d' `elem` [U, D]
+                    else d' `elem` [L, R]
+                tt
+              SPipe vert -> do
+                guard
+                  if expectVertical
+                    then vert
+                    else not vert
+                tt
+      let connInfo@[connU, connL, connD, connR] = fmap conn [U, L, D, R]
+          errConn =
+            error $
+              "unexpected connectivity on " <> show (coord, sym) <> " : " <> show connInfo
+          checkAndReturn conns = case sequence conns of
+            Just ds -> pure (coord, ds)
+            Nothing -> errConn
+          handlePipe vert =
+            {-
+              pipes only care about its own directions, otherwise cases with curves would be tricky,
+              e.g.
+               \
+              ---
+               /
+             -}
+            checkAndReturn (if vert then [connU, connD] else [connL, connR])
+          handleCurve connAlt0 connAlt1 = do
+            let [alt0, alt1] = fmap sequence [connAlt0, connAlt1]
+            -- make sure it's mutual exclusive.
+            case (alt0, alt1) of
+              (Nothing, Just ds) -> pure (coord, ds)
+              (Just ds, Nothing) -> pure (coord, ds)
+              (_, _) -> errConn
+      case sym of
+        SCurve False ->
+          -- '\' connects either U,R or L,D,
+          handleCurve [connU, connR] [connL, connD]
+        SCurve True ->
+          -- '/' connects either U,L or D,R, make sure it's mutual exclusive.
+          handleCurve [connU, connL] [connD, connR]
+        SIntersect ->
+          checkAndReturn connInfo
+        SPipe vert -> handlePipe vert
+        SCart dir -> handlePipe (dir `elem` [U, D])
+    syms :: M.Map Coord Sym
+    syms = M.fromList do
+      (y, rs) <- zip [0 ..] raw
+      (x, ch) <- zip [0 ..] rs
+      Just s <- pure $ parseSym ch
+      pure ((x, y), s)
+
 tickCart :: MapInfo -> CartState -> CartState
 tickCart
   MapInfo {miGraph, miCurves}
@@ -104,7 +160,10 @@ tickCart
        in CartState {csLoc, csDir = f oldDir, csTurnOpt}
     | Just curveType <- atCurve =
       let csDir =
-            -- TODO: need some clarification here as this is indeed very confusing.
+            {-
+              Determines how cart's direction is changed when it hits a curve:
+              oldDir is the direction that we are moving in.
+             -}
             case curveType of
               False ->
                 -- '\'
@@ -134,98 +193,10 @@ tickCart
         where
           newLoc = applyDir oldDir oldLoc
 
-parseSym :: Char -> Maybe Sym
-parseSym = \case
-  ' ' -> Nothing
-  '\\' -> Just $ SCurve False
-  '/' -> Just $ SCurve True
-  '+' -> Just $ SIntersect
-  '^' -> Just $ SCart U
-  '<' -> Just $ SCart L
-  'v' -> Just $ SCart D
-  '>' -> Just $ SCart R
-  '-' -> Just $ SPipe False
-  '|' -> Just $ SPipe True
-  s -> error $ "Unknown: " <> show s
+-- carts are identified by its current location.
+type CartSimState = M.Map Coord CartState
 
-parseFromRaw :: [String] -> MapInfo
-parseFromRaw raw = MapInfo {miGraph, miCurves, miCarts}
-  where
-    miCurves = M.fromList do
-      (coord, SCurve v) <- M.toList syms
-      pure (coord, v)
-    miCarts = do
-      (coord, SCart d) <- M.toList syms
-      pure (coord, d)
-    miGraph = M.fromList do
-      (coord, sym) <- M.toList syms
-      let -- checks connectivity from current coord to a neighborhood.
-          conn dir = case syms M.!? coord' of
-            Nothing -> False
-            Just s' -> case s' of
-              SCurve _ -> True
-              SIntersect -> True
-              SCart d' ->
-                if expectVertical
-                  then d' `elem` [U, D]
-                  else d' `elem` [L, R]
-              SPipe vert -> if expectVertical then vert else not vert
-            where
-              expectVertical = dir `elem` [U, D]
-              coord' = applyDir dir coord
-      -- TODO: there got to be a better way of writing this ...
-      let connInfo@[u, l, d, r] = fmap conn [U, L, D, R]
-          errConn =
-            error $
-              "unexpected connectivity on " <> show (coord, sym) <> " : " <> show connInfo
-          handlePipe vert =
-            {-
-              pipes only care about its own directions, otherwise cases with curves would be tricky,
-              e.g.
-               \
-              ---
-               /
-             -}
-            if vert
-              then
-                (if u && d
-                   then pure (coord, fmap (`applyDir` coord) [U, D])
-                   else errConn)
-              else
-                (if l && r
-                   then pure (coord, fmap (`applyDir` coord) [L, R])
-                   else errConn)
-      case sym of
-        SCurve False ->
-          -- '\' connects either U,R or L,D, make sure it's mutual exclusive.
-          if
-              | u && r && not (l && d) ->
-                pure (coord, fmap (`applyDir` coord) [U, R])
-              | not (u && r) && l && d ->
-                pure (coord, fmap (`applyDir` coord) [L, D])
-              | otherwise -> errConn
-        SCurve True ->
-          -- '/' connects either U,L or D,R, make sure it's mutual exclusive.
-          if
-              | u && l && not (d && r) ->
-                pure (coord, fmap (`applyDir` coord) [U, L])
-              | not (u && l) && d && r ->
-                pure (coord, fmap (`applyDir` coord) [D, R])
-              | otherwise -> errConn
-        SIntersect -> do
-          if and connInfo
-            then pure (coord, fmap (`applyDir` coord) [U, L, D, R])
-            else errConn
-        SPipe vert -> handlePipe vert
-        SCart dir -> handlePipe (dir `elem` [U, D])
-    syms :: M.Map Coord Sym
-    syms = M.fromList do
-      (y, rs) <- zip [0 ..] raw
-      (x, ch) <- zip [0 ..] rs
-      Just s <- pure $ parseSym ch
-      pure ((x, y), s)
-
-type CartSim = StateT (M.Map Coord CartState) (Either Coord)
+type CartSim = StateT CartSimState (Either Coord)
 
 {-
   TODO: it might be possible that we recover from an error and continue
@@ -234,8 +205,18 @@ type CartSim = StateT (M.Map Coord CartState) (Either Coord)
  -}
 tick :: Bool -> MapInfo -> CartSim ()
 tick stopOnConflict mi = do
+  {-
+    We need to make sure y goes first, then x.
+    One alternative is that we can make Coord a newtype and customize its Ord
+    instance. This way we can just use toAscList.
+    But I feel that's too much effort for too little benefit.
+   -}
   carts <- gets (sortOn (\((x, y), _) -> (y, x)) . M.toList)
   forM_ carts $ \(cartLoc, cartState) -> do
+    {-
+      As in part 2 we are asked to continue the simulate even when carts crash,
+      a cart might not exist anymore when we get to it in current tick.
+     -}
     exist <- gets (M.member cartLoc)
     when exist do
       modify (M.delete cartLoc)
@@ -248,6 +229,12 @@ tick stopOnConflict mi = do
           modify (M.delete newLoc)
         Nothing ->
           modify (M.insert newLoc cartState')
+  {-
+    Note that this is outside of the for loop,
+    as the puzzle asks us to check after a tick is done,
+    meaning even if there's only one cart remaining,
+    we should tick it first before reporting its location.
+   -}
   sz <- gets M.size
   when (sz == 1) do
     ~[(c, _)] <- gets M.toList
