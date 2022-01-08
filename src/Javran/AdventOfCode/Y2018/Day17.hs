2d092eb42cb37386231b78b97b8fd4a0a4396d65
Javran Cheng <Javran.C@gmail.com>
2022-01-07 20:14:52 -0800

    some cleanup.

--- src/Javran/AdventOfCode/Y2018/Day17.hs
+++ src/Javran/AdventOfCode/Y2018/Day17.hs
@@ -1,53 +1,29 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2018.Day17
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
 import Control.Monad
 import Control.Monad.State.Strict
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
-import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
-import Data.Monoid
-import Data.Semigroup
 import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (count, get, many)
 
 data Day17 deriving (Generic)
 
 type Coord = (Int, Int) -- x, y
+
+type CoordSet = S.Set Coord
 
 inputLineP :: ReadP [Coord]
 inputLineP = do
@@ -57,7 +33,7 @@ inputLineP = do
      )
        <$ char 'x')
       <++ (( 'x'
-           , (\y xs -> [(x, y) | x <- xs])
+           , \y xs -> [(x, y) | x <- xs]
            )
              <$ char 'y')
   _ <- char '='
@@ -69,66 +45,63 @@ inputLineP = do
   pure (mk u [vFrom .. vTo])
 
 {-
-  TODO: I suspect we are looking at some simulation again:
+  State for a simulation approach: we maintain a set of springs
+  and simulate as they go downwards.
+  Whenever they hit something "solid" (either clay or still water are considered so).
+  and there is no way for the spring to continue going down,
+  the simulation goes back up, filling still water until springs can go downwards again.
 
-  - keep a list of x of springs (resulting from branching the very first one),
-    and y-coord that we are currently scanning.
-  - moving down until we "hit" something, set `|` mark appropriately.
-  - now we have few situations to deal with
-
-    + if we cannot find bound on either side, find locations that we can
-      branch this spring and keep going down, set `|` mark appropriately.
-
-    + if we find bound one side, set `|` mark appropriately,
-      and "redirect" current spring, as we are leaking from only one side.
-
-    + if we find bound both sides, set `~` as appropriate, then
-      we have to move y back up instead of down to "fill the reservoir"
-
- -}
-
-{-
-  a coord could be in both, wsReached and wsStayed.
-  in which case that coord is considered stayed.
+  A coord of water could be in both wsReached and wsStayed,
+  in which case the water on that coord is considered stayed (i.e. still water).
  -}
 data WaterState = WaterState
-  { wsSpring :: S.Set Coord
+  { wsSpring :: CoordSet
   , wsScanY :: Int -- current scanning line, only coord of that y-coord is considered.
-  , wsReached :: S.Set Coord
-  , wsStayed :: S.Set Coord
+  , wsReached :: CoordSet
+  , wsStayed :: CoordSet
   }
 
-simulate :: S.Set Coord -> Int -> State WaterState ()
-simulate clay yMax = do
-  y <- gets wsScanY
-  when (y <= yMax) do
-    xs <- gets (concatMap (\(x', y') -> [x' | y' == y - 1]) . S.toList . wsSpring)
-    shouldGoUp <- forM xs \x -> do
-      exist <- gets ((S.member (x, y -1)) . wsSpring)
-      if exist
-        then simulateAux clay x
-        else pure False
-    modify (\ws@WaterState {wsScanY} -> ws {wsScanY = if or shouldGoUp then wsScanY -1 else wsScanY + 1})
-    simulate clay yMax
-
 {-
-  considers a single spring x, processes it and return whether we want to "rewind" y.
-  (True means we want to rewind)
+  Considers a single spring at (x, wsScanY-1), processes scaning line wsScanY and
+  makes a decision about which direction should we go next:
+  - True: we hit some solid and current scanning line is filled,
+    and we need to go back up.
+  - False: this spring will be able to go downwards without obstacle.
  -}
-simulateAux :: S.Set Coord -> Int -> State WaterState Bool
-simulateAux clay x = do
+simulateOneSpring :: S.Set Coord -> Int -> State WaterState Bool
+simulateOneSpring clay x = do
   y <- gets wsScanY
   (isSolid :: Coord -> Bool) <-
     gets
-      ((\stayed coord -> S.member coord stayed || S.member coord clay)
+      ((\stayed coord ->
+          {-
+            either clay or still water can stop the spring,
+            causing it to move horizontally.
+           -}
+          S.member coord stayed || S.member coord clay)
          . wsStayed)
   let curSpring = (x, y -1)
+      replaceSpring xs =
+        modify
+          (\ws@WaterState {wsSpring} ->
+             ws {wsSpring = foldr S.insert (S.delete curSpring wsSpring) xs})
+      markReached xs = modify (\ws@WaterState {wsReached} -> ws {wsReached = foldr S.insert wsReached xs})
+      markStayed xs = modify (\ws@WaterState {wsStayed} -> ws {wsStayed = foldr S.insert wsStayed xs})
   if isSolid (x, y + 1)
     then do
-      -- solid, now we want to reach both left and right.
-      let reaching next curX =
+      -- spring is hitting something solid, now we want to reach both left and right.
+      let {-
+            reaching to leftmost or rightmost on row y,
+            where spring will either be able to continue downwards or
+            be blocked.
+
+            Returns (<farthest>, <is open?>), where <farthest> indicates the leftmost
+            or rightmost that water can reach, and <is open?> indicates whether
+            spring can go downwards.
+           -}
+          reaching next curX =
             if
-                | isSolid (nextX, y) -> (curX, False) -- (<farthest reach>, <is open?>)
+                | isSolid (nextX, y) -> (curX, False)
                 | isSolid (nextX, y + 1) -> reaching next nextX
                 | otherwise -> (nextX, True)
             where
@@ -136,55 +109,49 @@ simulateAux clay x = do
           (leftmost, openL) = reaching pred x
           (rightmost, openR) = reaching succ x
           newCoords = [(x', y) | x' <- [leftmost .. rightmost]]
-          markReached = modify (\ws@WaterState {wsReached} -> ws {wsReached = foldr S.insert wsReached newCoords})
       case (openL, openR) of
         (False, False) -> do
           -- both end closed, we need to go back up.
-          modify (\ws@WaterState {wsSpring} -> ws {wsSpring = S.insert (x, y -2) $ S.delete curSpring wsSpring})
-          True <$ modify (\ws@WaterState {wsStayed} -> ws {wsStayed = foldr S.insert wsStayed newCoords})
+          -- (x, y-1) ==> (x, y-1)
+          replaceSpring [(x, y -2)]
+          True <$ markStayed newCoords
         (True, True) -> do
-          markReached
+          {-
+            It should be possible to generalize cases other than (False, False),
+            but I feel it's most readable to call it out what to do for all 3 cases.
+           -}
+          markReached newCoords
           -- both open, split the spring.
-          False
-            <$ modify
-              (\ws@WaterState {wsSpring} ->
-                 ws
-                   { wsSpring =
-                       S.insert (rightmost, y)
-                         . S.insert (leftmost, y)
-                         . S.delete curSpring
-                         $ wsSpring
-                   })
+          False <$ replaceSpring [(leftmost, y), (rightmost, y)]
         (False, True) -> do
-          markReached
+          markReached newCoords
           -- left closed, right open, move to right.
-          False
-            <$ modify
-              (\ws@WaterState {wsSpring} ->
-                 ws
-                   { wsSpring =
-                       S.insert (rightmost, y)
-                         . S.delete curSpring
-                         $ wsSpring
-                   })
+          False <$ replaceSpring [(rightmost, y)]
         (True, False) -> do
-          markReached
-          False
-            <$ modify
-              (\ws@WaterState {wsSpring} ->
-                 ws
-                   { wsSpring =
-                       S.insert (leftmost, y)
-                         . S.delete curSpring
-                         $ wsSpring
-                   })
+          -- left open, right closed, move to left.
+          markReached newCoords
+          False <$ replaceSpring [(leftmost, y)]
     else do
-      -- not solid, mark reached and move on
-      modify (\ws@WaterState {wsSpring} -> ws {wsSpring = S.insert (x, y) $ S.delete curSpring wsSpring})
+      -- spring is not hiting anything solid, mark it reached and carry current spring downwards.
+      -- (x, y-1) ==> (x, y)
+      replaceSpring [(x, y)]
       False
         <$ modify
           (\ws@WaterState {wsReached} ->
              ws {wsReached = S.insert (x, y) wsReached})
+
+simulate :: S.Set Coord -> Int -> State WaterState ()
+simulate clay yMax = do
+  y <- gets wsScanY
+  when (y <= yMax) do
+    xs <- gets (concatMap (\(x', y') -> [x' | y' == y - 1]) . S.toList . wsSpring)
+    shouldGoUp <- forM xs \x -> do
+      exist <- gets (S.member (x, y -1) . wsSpring)
+      if exist
+        then simulateOneSpring clay x
+        else pure False
+    modify (\ws@WaterState {wsScanY} -> ws {wsScanY = if or shouldGoUp then wsScanY -1 else wsScanY + 1})
+    simulate clay yMax
 
 pprWaterState :: S.Set Coord -> WaterState -> IO ()
 pprWaterState clay WaterState {wsStayed, wsReached} = do
@@ -196,7 +163,7 @@ pprWaterState clay WaterState {wsStayed, wsReached} = do
               | S.member (x, y) wsStayed -> '~'
               | S.member (x, y) wsReached -> '|'
               | otherwise -> ' '
-    putStrLn $ "<<" <> (fmap render [xMin .. xMax]) <> ">>"
+    putStrLn $ "<<" <> fmap render [xMin .. xMax] <> ">>"
 
 instance Solution Day17 where
   solutionRun _ SolutionContext {getInputS, answerShow} = do
