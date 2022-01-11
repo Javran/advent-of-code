14c90622bc450777ad9b5bdc19cd93158dfbf6db
Javran Cheng <Javran.C@gmail.com>
2022-01-10 20:28:48 -0800

    p1 done.
    can anyone tell me what exactly is the fun in this bullshit?

--- src/Javran/AdventOfCode/Y2018/Day24.hs
+++ src/Javran/AdventOfCode/Y2018/Day24.hs
@@ -27,6 +27,8 @@ where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Loops (untilJust)
+import Control.Monad.State.Strict
 import Data.Char
 import Data.Coerce
 import Data.Function
@@ -51,6 +53,13 @@ data Day24 deriving (Generic)
 newtype DamageType = DamageType Int deriving (Show)
 
 newtype ImmuneWeak = ImmuneWeak (IS.IntSet, IS.IntSet) deriving (Show)
+
+data Side = ImmuneSystem | Infection deriving (Eq, Show)
+
+opposite :: Side -> Side
+opposite = \case
+  ImmuneSystem -> Infection
+  Infection -> ImmuneSystem
 
 data ArmyGroup = ArmyGroup
   { agCount :: Int
@@ -94,7 +103,7 @@ armyGroupP = do
           }
   pure (mkArmy, S.unions [immunes, weaks, S.singleton dmgTy])
 
-inputP :: ReadP (([ArmyGroup], [ArmyGroup]), DamageType -> String)
+inputP :: ReadP (CombatState, DamageType -> String)
 inputP = do
   let nl = void (char '\n')
   _ <- string "Immune System:\n"
@@ -107,17 +116,28 @@ inputP = do
       revDmgTypes = M.fromList (zip allDmgTypes [0 ..])
       sToI = DamageType . (revDmgTypes M.!)
       iToS (DamageType i) = dmgTypes V.! i
-  pure ((fmap ($ sToI) immuneAgs, fmap ($ sToI) infectionAgs), iToS)
+      allArmyGroups =
+        fmap (\g -> (ImmuneSystem, g sToI)) immuneAgs
+          <> fmap (\g -> (Infection, g sToI)) infectionAgs
+  pure
+    ( IM.fromListWith (error "initiative conflict") $
+        fmap (\p@(_, g) -> (agInit g, p)) allArmyGroups
+    , iToS
+    )
 
-pprParsed ((xs, ys), iToS) = do
-  let pprGroup
+pprParsed (combat, iToS) = do
+  let (xs, ys) =
+        bimap (fmap snd) (fmap snd) $
+          partition ((== ImmuneSystem) . fst) $
+            fmap snd $ IM.toDescList combat
+      pprGroup
         ArmyGroup
           { agCount
           , agHp
           , agImmuneWeak =
             ImmuneWeak (im, wk)
           , agAttack = (atkVal, atkTy)
-          , agInit
+          , agInit = i
           } = do
           printf
             "  Units: %d, Hps: %d, Attack: %d %s, Init: %d\n"
@@ -125,7 +145,7 @@ pprParsed ((xs, ys), iToS) = do
             agHp
             atkVal
             (iToS atkTy)
-            agInit
+            i
           unless (IS.null im) do
             putStrLn $
               "    Immune to: "
@@ -139,8 +159,119 @@ pprParsed ((xs, ys), iToS) = do
   putStrLn "Infection:"
   mapM_ pprGroup ys
 
+effectivePower :: ArmyGroup -> Int
+effectivePower ArmyGroup {agCount, agAttack = (dmg, _)} = agCount * dmg
+
+{-
+  Looks like it's safe to identify a group by its initiative.
+ -}
+type CombatState = IM.IntMap (Side, ArmyGroup)
+
+damageFactor :: DamageType -> ImmuneWeak -> Int
+damageFactor (DamageType dt) (ImmuneWeak (im, wk))
+  | IS.member dt im = 0
+  | IS.member dt wk = 2
+  | otherwise = 1
+
+{-
+  return values:
+  - Nothing: cannot deal any damage
+  - Just _: can deal damage
+ -}
+dealDamage :: ArmyGroup -> ArmyGroup -> Maybe Int
+dealDamage atkGrp@ArmyGroup {agAttack = (_, atkTy)} ArmyGroup {agImmuneWeak = defImWk} = do
+  let damageDealt = effectivePower atkGrp * damageFactor atkTy defImWk
+  {-
+    Note that it's unclear what to do
+    if a positive amount of damage can be dealt,
+    but it won't result in any unit loss.
+   -}
+  damageDealt <$ guard (damageDealt > 0)
+
+selectTarget :: ArmyGroup -> [ArmyGroup] -> Maybe ArmyGroup
+selectTarget atkGrp tgts0 = do
+  let tgts1 =
+        mapMaybe
+          (\defGrp ->
+             dealDamage atkGrp defGrp >>= \dmgDlt -> Just (dmgDlt, defGrp))
+          tgts0
+      sortedTgts =
+        sortOn
+          (\(dmgDlt, defGrp@ArmyGroup {agInit}) ->
+             ( Down dmgDlt
+             , Down $ effectivePower defGrp
+             , Down agInit
+             ))
+          tgts1
+  snd <$> listToMaybe sortedTgts
+
+targetSelectionPhase :: State CombatState [(Int, Maybe Int)]
+targetSelectionPhase = do
+  allGrps <- get
+  let initAtkGrps =
+        sortOn
+          (\(_side, g) ->
+             (Down (effectivePower g), Down (agInit g)))
+          (IM.elems allGrps)
+
+  fix
+    (\loop atkGrps selectables selected ->
+       case atkGrps of
+         [] -> pure (sortOn (Down . fst) selected)
+         (atkSide, atkGrp) : atkGrps' ->
+           let defGrps = concatMap (\(s, g) -> [g | s == opposite atkSide]) $ IM.elems selectables
+               sel = selectTarget atkGrp defGrps
+               selectables' = case sel of
+                 Nothing -> selectables
+                 Just ArmyGroup {agInit = i} -> IM.delete i selectables
+            in loop atkGrps' selectables' ((agInit atkGrp, agInit <$> sel) : selected))
+    initAtkGrps
+    allGrps
+    []
+
+attack :: ArmyGroup -> ArmyGroup -> Maybe ArmyGroup
+attack atkGrp defGrp@ArmyGroup {agCount = defCnt, agHp = defHp} = do
+  let Just dmgDealt =
+        -- target selection is at fault if we cannot deal any damage.
+        dealDamage atkGrp defGrp
+      unitLost = dmgDealt `quot` defHp
+      agCount' = defCnt - unitLost
+  guard $ agCount' > 0
+  pure defGrp {agCount = agCount'}
+
+attackingPhase :: [(Int, Maybe Int)] -> State CombatState (Maybe Side)
+attackingPhase actions = do
+  forM_ actions \(atk, mDef) -> do
+    st <- get
+    let mAtkDef = do
+          attacker <- st IM.!? atk
+          defenderInit <- mDef
+          defender <- st IM.!? defenderInit
+          pure (attacker, defender)
+    case mAtkDef of
+      Nothing -> pure ()
+      Just ((_aSide, atkGrp), (dSide, defGrp)) -> do
+        let mDefGrp = attack atkGrp defGrp
+        modify (IM.update (\_ -> (dSide,) <$> mDefGrp) (agInit defGrp))
+  do
+    st <- get
+    let (xs, ys) = partition ((== ImmuneSystem) . fst) $ IM.elems st
+    pure
+      if
+          | null xs -> Just Infection
+          | null ys -> Just ImmuneSystem
+          | otherwise -> Nothing
+
+simulate = untilJust (targetSelectionPhase >>= attackingPhase)
+
 instance Solution Day24 where
   solutionSolved _ = False
   solutionRun _ SolutionContext {getInputS, answerShow} = do
-    inp <- consumeOrDie inputP <$> getInputS
+    inp@(st, _) <- consumeOrDie inputP <$> getInputS
+    let (r, s) = runState (simulate) st
     pprParsed inp
+    print r
+    forM_ (IM.elems s) $ \(side, g) -> do
+      print (side, agCount g)
+    let ans = sum $ fmap (agCount . snd) $ IM.elems s
+    answerShow ans
