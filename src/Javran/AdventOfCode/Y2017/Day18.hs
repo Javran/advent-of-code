7c9c30cc51cbaa88022d20232c3458f7589d9312
Javran Cheng <Javran.C@gmail.com>
2022-01-14 23:20:03 -0800

    going fancy, which may or may not work.

--- src/Javran/AdventOfCode/Y2017/Day18.hs
+++ src/Javran/AdventOfCode/Y2017/Day18.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -12,6 +14,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
 {-# OPTIONS_GHC -Wno-typed-holes #-}
@@ -27,6 +30,7 @@ where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Writer.CPS
 import Data.Char
 import Data.Function
 import Data.Function.Memoize (memoFix)
@@ -37,9 +41,11 @@ import Data.List.Split hiding (sepBy)
 import qualified Data.Map.Strict as M
 import Data.Monoid
 import Data.Semigroup
+import qualified Data.Sequence as Seq
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import Data.Void
 import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
 import Text.ParserCombinators.ReadP hiding (count, get, many)
@@ -79,6 +85,9 @@ instrP =
         <$> (satisfy isAsciiLower <* skipSpaces)
     readValP = (Right <$> (regP <* skipSpaces)) <++ (Left <$> intP)
 
+registerP :: Reg
+registerP = Reg $ ord 'p' - ord 'a'
+
 {- ((pc, regs), last sent) -}
 type Machine = ((Int, IM.IntMap Int), Maybe Int)
 
@@ -116,6 +125,72 @@ solve instrs st@((pc, _), _) = case interpret instrs st of
   Just st'@(_, sent) -> case instrs V.! pc of
     Recv _ -> fromJust sent
     _ -> solve instrs st'
+
+data ExtInt = External | Internal
+
+type family InternalOnly ei where
+  InternalOnly 'Internal = ()
+  InternalOnly 'External = Void
+
+data Result (ei :: ExtInt)
+  = Done Machine2
+  | Paused (InternalOnly ei) (Result ei)
+  | NeedInput (Int -> Result ei)
+  | SentOutput Int (Result ei)
+
+type Machine2 = (Int, IM.IntMap Int)
+
+tick :: V.Vector Instr -> Machine2 -> Result 'Internal
+tick instrs m@(pc, regs) =
+  if (pc < 0 || pc >= V.length instrs)
+    then Done m
+    else
+      let getVal :: ReadVal -> Int
+          getVal = \case
+            Left v -> v
+            Right (Reg i) -> fromMaybe 0 (regs IM.!? i)
+          liftOp op rx@(Reg x) y =
+            Paused
+              ()
+              (tick
+                 instrs
+                 ( pc + 1
+                 , IM.insert x (op (getVal (Right rx)) (getVal y)) regs
+                 ))
+       in case instrs V.! pc of
+            Send x -> SentOutput (getVal x) $ tick instrs (pc + 1, regs)
+            Assign rx y -> liftOp (\_ y' -> y') rx y
+            Add rx y -> liftOp (+) rx y
+            Mul rx y -> liftOp (*) rx y
+            Mod rx y -> liftOp rem rx y
+            Recv (Reg r) -> NeedInput \inp ->
+              tick instrs (pc + 1, IM.insert r inp regs)
+            JumpGtZero x y ->
+              let x' = getVal x
+                  y' = getVal y
+               in Paused () (tick instrs $ if x' > 0 then (pc + y', regs) else (pc + 1, regs))
+
+ticks :: Result 'Internal -> Result 'External
+ticks = \case
+  Done m -> Done m
+  Paused () k -> ticks k
+  NeedInput k -> NeedInput (ticks . k)
+  SentOutput o k -> SentOutput o (ticks k)
+
+type System = ((Result 'External, Seq.Seq Int), (Result 'External, Seq.Seq Int))
+
+runDuet :: System -> Writer (Sum Int) ()
+runDuet ((r0, q0), (r1, q1)) = case ((r0, q0), (r1, q1)) of
+  ((Paused v _, _), _) -> absurd v
+  (_, (Paused v _, _)) -> absurd v
+  ((SentOutput o r0', _), (r1', _)) -> runDuet ((r0', q0), (r1', q1 Seq.|> o))
+  ((r0', _), (SentOutput o r1', _)) -> tell 1 >> runDuet ((r0', q0 Seq.|> o), (r1', q1))
+  ((Done {}, _), (Done {}, _)) -> pure ()
+  ((NeedInput k0, i Seq.:<| q0'), _) -> runDuet ((k0 i, q0'), (r1, q1))
+  (_, (NeedInput k1, i Seq.:<| q1')) -> runDuet ((r0, q0), (k1 i, q1'))
+  ((NeedInput {}, Seq.Empty), (Done {}, _)) -> pure ()
+  ((Done {}, _), (NeedInput {}, Seq.Empty)) -> pure ()
+  ((NeedInput {}, Seq.Empty), (NeedInput {}, Seq.Empty)) -> pure ()
 
 instance Solution Day18 where
   solutionSolved _ = False
