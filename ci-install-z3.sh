1017a335075d9ab1ee5e38e4d2439edef8c6db6c
Javran Cheng <Javran.C@gmail.com>
2022-01-17 02:08:17 -0800

    probably don't have to copy after all.

--- ci-install-z3.sh
+++ ci-install-z3.sh
@@ -11,19 +11,9 @@ curl -L "https://github.com/Z3Prover/z3/releases/download/${Z3_RELEASE}/${Z3_LON
 
 pushd "$HOME" >/dev/null
 unzip "$zip_target"
-
-cd "$Z3_LONG_VERSION"
-
-sudo cp -v bin/*{.a,.so} /usr/lib/
-sudo cp -v bin/*{.a,.so} /usr/lib64/
-sudo cp -v bin/z3 /usr/bin/
-sudo cp -v include/* /usr/include/
-
-popd
+popd >/dev/null
 
 echo extra-include-dirs: >>stack.yaml
 echo - "$HOME/$Z3_LONG_VERSION/include" >>stack.yaml
 echo extra-lib-dirs: >>stack.yaml
 echo - "$HOME/$Z3_LONG_VERSION/bin" >>stack.yaml
-
-cat stack.yaml
