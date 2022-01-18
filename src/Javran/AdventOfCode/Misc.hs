5a760386ddf57e1832fa88128c15268e1e694483
Javran Cheng <Javran.C@gmail.com>
2022-01-18 00:03:39 -0800

    util for internalization.

--- src/Javran/AdventOfCode/Misc.hs
+++ src/Javran/AdventOfCode/Misc.hs
@@ -1,8 +1,14 @@
 module Javran.AdventOfCode.Misc
   ( rotateLeftBy
   , rotateRightBy
+  , internalize
   )
 where
+
+import qualified Data.Array as Arr
+import Data.Containers.ListUtils
+import qualified Data.Map.Strict as M
+import Data.Tuple
 
 {-
   This module contains things that could probably be in Prelude,
@@ -19,3 +25,14 @@ where
 rotateLeftBy, rotateRightBy :: Int -> Int -> [a] -> [a]
 rotateLeftBy n offset xs = take n $ drop offset $ cycle xs
 rotateRightBy n offset = rotateLeftBy n (n - offset)
+
+{-
+  Builds one-to-one mappings between a known set of values (typically Strings)
+  and Int.
+ -}
+internalize :: Ord a => [a] -> (a -> Int, Int -> a)
+internalize xs = ((m M.!), (arr Arr.!))
+  where
+    paired = zip (nubOrd xs) [0 ..]
+    m = M.fromList paired
+    arr = Arr.array (0, M.size m - 1) (fmap swap paired)
