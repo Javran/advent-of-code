a0fee54138d890cd6ffe69f06466a16d4e7f126e
Javran Cheng <Javran.C@gmail.com>
2022-01-20 21:13:21 -0800

    cleanup, downscale test input.

--- src/Javran/AdventOfCode/Y2016/Day16.hs
+++ src/Javran/AdventOfCode/Y2016/Day16.hs
@@ -1,58 +1,29 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-deprecations #-}
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Javran.AdventOfCode.Y2016.Day16
   (
   )
 where
 
-{- HLINT ignore -}
-
-import Control.Applicative
-import Control.Monad
-import Data.Char
-import Data.Function
-import Data.Function.Memoize (memoFix)
-import qualified Data.IntMap.Strict as IM
-import qualified Data.IntSet as IS
-import Data.List
-import Data.List.Split hiding (sepBy)
-import qualified Data.Map.Strict as M
-import Data.Monoid
-import Data.Semigroup
-import qualified Data.Set as S
-import qualified Data.Text as T
 import qualified Data.Vector as V
-import GHC.Generics (Generic)
 import Javran.AdventOfCode.Prelude
-import Javran.AdventOfCode.TestExtra (consumeExtra)
+import Javran.AdventOfCode.TestExtra
 import Text.ParserCombinators.ReadP hiding (count, get, many)
 
 data Day16 deriving (Generic)
 
-type DragonSeq = (Int, Int -> Bool)
+{-
+  A virtual sequence.
+  consists of only its length and how to access its elements.
+ -}
+type VSeq = (Int, Int -> Bool)
 
 bitP :: ReadP Bool
 bitP = (False <$ char '0') <++ (True <$ char '1')
 
-gen :: DragonSeq -> DragonSeq
+gen :: VSeq -> VSeq
 gen (l, getAt) = (l * 2 + 1, f)
   where
     f i = case compare i l of
@@ -60,32 +31,39 @@ gen (l, getAt) = (l * 2 + 1, f)
       EQ -> False
       GT -> not (getAt (2 * l - i))
 
-stepChecksum :: [Bool] -> [Bool]
-stepChecksum = fmap (\(~[a, b]) -> a == b) . chunksOf 2
+stepChecksum :: VSeq -> VSeq
+stepChecksum (l, getAt) =
+  ( halve l
+  , \i ->
+      let i' = i * 2
+       in getAt i' == getAt (i' + 1)
+  )
 
-computeChecksum :: Int -> [Bool] -> [Bool]
-computeChecksum l xs = aux (halve l) (stepChecksum xs)
+computeChecksum :: VSeq -> VSeq
+computeChecksum =
+  head . dropWhile (even . fst) . tail . iterate stepChecksum
+
+ppr :: VSeq -> String
+ppr (l, getAt) = fmap (bool '0' '1' . getAt) [0 .. l -1]
+
+solve :: Int -> VSeq -> VSeq
+solve fillLen initSeq = computeChecksum (fillLen, getAt)
   where
-    aux curL ys =
-      if odd curL
-        then ys
-        else computeChecksum curL ys
+    (_, getAt) : _ = dropWhile ((< fillLen) . fst) $ iterate gen initSeq
 
 instance Solution Day16 where
   solutionRun _ SolutionContext {getInputS, answerS} = do
     (extraOpts, rawInput) <- consumeExtra getInputS
     let xs = V.fromList . consumeOrDie (many1 bitP) . head . lines $ rawInput
-        initSeq :: DragonSeq
+        initSeq :: VSeq
         initSeq = (V.length xs, (xs V.!))
     do
       let fillLen = case extraOpts of
             Nothing -> 272
             Just ~[raw] -> read raw
-          (_, getAt) : _ = dropWhile ((< fillLen) . fst) $ iterate gen initSeq
-          ys = computeChecksum fillLen $ fmap getAt [0 .. fillLen -1]
-      answerS $ fmap (bool '0' '1') ys
+      answerS $ ppr $ solve fillLen initSeq
     do
-      let fillLen = 35651584
-          (_, getAt) : _ = dropWhile ((< fillLen) . fst) $ iterate gen initSeq
-          ys = computeChecksum fillLen $ fmap getAt [0 .. fillLen -1]
-      answerS $ fmap (bool '0' '1') ys
+      let fillLen = case extraOpts of
+            Nothing -> 35651584
+            Just _ -> 0x70000
+      answerS $ ppr $ solve fillLen initSeq
