4195a59e180d0a82df7749be28afba450507fb7e
Javran Cheng <Javran.C@gmail.com>
2022-02-01 23:40:44 -0800

    now we can get back to "e".

--- src/Javran/AdventOfCode/Y2015/Day19.hs
+++ src/Javran/AdventOfCode/Y2015/Day19.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -30,6 +31,7 @@ import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Char8 as BSC
 import Data.Char
+import qualified Data.DList as DL
 import Data.Function
 import Data.Function.Memoize (memoFix)
 import qualified Data.HashMap.Strict as HM
@@ -80,30 +82,13 @@ performReplace rules inp = do
 
 type Rules = [(Str, [Str])]
 
-psearch rules steps q0 = case PQ.minView q0 of
-  Nothing -> error "queue exhausted"
-  Just (cur PQ.:-> Arg _l step, q1) ->
-    if cur == "e"
-      then step
-      else
-        let nexts = do
-              next <- performReplace rules cur
-              let step' = step + 1
-              guard case HM.lookup next steps of
-                Nothing -> True
-                Just v -> v > step'
-              pure (next, step')
-            steps' = foldr (\(next, step') -> HM.insert next step') steps nexts
-            q2 = foldr (\(next, step') -> PQ.insert next (Arg (BSC.length next) step')) q1 nexts
-         in psearch rules steps' q2
-
 type Atom = String
 
 data RnAr a
   = Y0 a
   | Y1 a a
   | Y2 a a a
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Functor)
 
 data Inp
   = Flat Atom
@@ -126,18 +111,20 @@ inpP = rnArP <++ (Flat <$> atomP)
         [a, b] -> Y1 a b
         ~[a, b, c] -> Y2 a b c
 
-replacementClosure rules = fix \go discovered acc -> \case
-  Seq.Empty -> acc
-  a Seq.:<| q1 ->
-    let nexts = performReplace rules a
+replacementClosure rules = fix \go discovered q0 -> case PQ.minView q0 of
+  Nothing -> []
+  Just (a PQ.:-> _l, q1) ->
+    let nexts = performReplace2 rules a
      in case nexts of
-          [] -> go discovered (S.insert a acc) q1
+          [] -> a : go discovered q1
           _ : _ ->
             let nexts' = do
                   next <- nexts
                   guard $ S.notMember next discovered
-                  pure next
-             in go (foldr S.insert discovered nexts') acc (q1 <> Seq.fromList nexts')
+                  pure (next, length next)
+                q2 = foldr (uncurry PQ.insert) q1 nexts'
+                discovered' = foldr (\(n, _) -> S.insert n) discovered nexts'
+             in go discovered' q2
 
 {-
   TODO:
@@ -176,48 +163,76 @@ rule2P = do
   rhsPre <- many1 inpP
   rhs <- case rhsPre of
     [Flat a, Flat b] -> pure $ Left (a, b)
-    [Flat a, Nest xs] -> (\v -> Right (a,v)) <$> (case xs of
-      Y0 [Flat x0] -> pure $ Y0 x0
-      Y1 [Flat x0] [Flat x1] -> pure $ Y1 x0 x1
-      Y2 [Flat x0] [Flat x1] [Flat x2] -> pure $ Y2 x0 x1 x2
-      _ -> pfail)
+    [Flat a, Nest xs] ->
+      (\v -> Right (a, v))
+        <$> (case xs of
+               Y0 [Flat x0] -> pure $ Y0 x0
+               Y1 [Flat x0] [Flat x1] -> pure $ Y1 x0 x1
+               Y2 [Flat x0] [Flat x1] [Flat x2] -> pure $ Y2 x0 x1 x2
+               _ -> pfail)
     _ -> pfail
   pure (lhs, rhs)
+
+splitAts2 :: [Inp] -> [] ([Inp], [Inp])
+splitAts2 xs = do
+  n <- [0 .. length xs]
+  pure $ splitAt n xs
+
+type Rules2 = M.Map [Inp] Atom
+
+performReplace2 :: Rules2 -> [Inp] -> [] [Inp]
+performReplace2 rules inp = do
+  (pre, xs0) <- splitAts2 inp
+  (lhs, rhs) <- M.toList rules
+  Just xs1 <- pure $ stripPrefix lhs xs0
+  pure $ pre <> [Flat rhs] <> xs1
+
+simpInp :: Rules2 -> Inp -> [Inp]
+simpInp rs = \case
+  x@Flat {} -> [x]
+  Nest rnAr -> case rnAr of
+    Y0 x0 -> do
+      x0' <- simp' x0
+      pure $ Nest $ Y0 x0'
+    Y1 x0 x1 -> do
+      x0' <- simp' x0
+      x1' <- simp' x1
+      pure $ Nest $ Y1 x0' x1'
+    Y2 x0 x1 x2 -> do
+      x0' <- simp' x0
+      x1' <- simp' x1
+      x2' <- simp' x2
+      pure $ Nest $ Y2 x0' x1' x2'
+  where
+    simp' i = do
+      [o] <- simp rs i
+      pure [o]
+
+simp :: Rules2 -> [Inp] -> [] [Inp]
+simp rs xs = do
+  xs' <- mapM (simpInp rs) xs
+  replacementClosure rs (S.singleton xs') (PQ.singleton xs' (length xs'))
 
 instance Solution Day19 where
   solutionSolved _ = False
   solutionRun _ SolutionContext {getInputS, answerShow} = do
     [rawRules, [inp]] <- splitOn [""] . lines <$> getInputS
     let inp' = BSC.pack inp
-        rules = M.toAscList $ M.fromListWith (<>) $ fmap tr rawRules
-          where
-            tr x =
-              let [a, b] = splitOn " => " x
-               in (BSC.pack a, [BSC.pack b])
-    answerShow (S.size $ S.fromList $ performReplace rules inp')
     do
-      let revRules1 :: Rules
-          revRules = M.toAscList $ M.fromListWith (<>) do
-            (lhs, rhss) <- rules
-            rhs <- rhss
-            pure (rhs, [lhs])
-          revRules1 = M.toAscList $ M.fromListWith (<>) do
-            (lhs, rhss) <- rules
-            rhs <- rhss
-            guard $ "Rn" `BSC.isInfixOf` rhs
-            pure (rhs, [lhs])
-          revRules2 = M.toAscList $ M.fromListWith (<>) do
-            (lhs, rhss) <- rules
-            rhs <- rhss
-            guard $ not $ "Rn" `BSC.isInfixOf` rhs
-            pure (rhs, [lhs])
-          experiment rs cur n = case performReplace rs cur of
-            [] -> (cur, n)
-            next : _ -> experiment rs next (n + 1)
-      printer (consumeOrDie (many inpP) inp)
-      let experiment2 i =
-            sortOn BSC.length $
-              S.toList $
-                replacementClosure revRules (S.singleton i) S.empty (Seq.singleton i)
-
-      mapM_ (print . consumeOrDie rule2P) rawRules
+      let rules = M.toAscList $ M.fromListWith (<>) $ fmap tr rawRules
+            where
+              tr x =
+                let [a, b] = splitOn " => " x
+                 in (BSC.pack a, [BSC.pack b])
+      answerShow (S.size $ S.fromList $ performReplace rules inp')
+    do
+      let rules :: Rules2
+          rules = M.fromListWith (error "duplicated key") do
+            rawRule <- rawRules
+            let (oLhs, oRhs) = consumeOrDie rule2P rawRule
+                lhs' = case oRhs of
+                  Left (a, b) -> [Flat a, Flat b]
+                  Right (a, ra) -> [Flat a, Nest $ fmap ((: []) . Flat) ra]
+            pure (lhs', oLhs)
+          inp2 = consumeOrDie (many inpP) inp
+      answerShow $ take 1 $ simp rules inp2
