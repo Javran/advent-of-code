143e6d67d080fe222027e7000810422bc91d5920
Javran Cheng <Javran.C@gmail.com>
2022-02-04 20:47:41 -0800

    more properties to check.
    it appears to be the case that some operations are indeed irreversible
    so it has no alternatives when unapplied - those cases go way if we
    check the consistency the other way around: applyOp first then
    unapplyOp should guarantee that we get the original input inside
    alternatives.

--- test/Javran/AdventOfCode/Y2016/Day21Spec.hs
+++ test/Javran/AdventOfCode/Y2016/Day21Spec.hs
@@ -59,6 +59,10 @@ spec :: Spec
 spec =
   describe "Y2016" do
     describe "Day21" do
+      let performOps n =
+            foldl' (\cur op -> applyOp n op cur)
+          unperformOps n result ops =
+            foldM (\cur op -> unapplyOp n op cur) result (reverse ops)
       prop "unapplyOp then applyOp is consistent" $ forAll
         (do
            n <- chooseInt (5, 10)
@@ -71,15 +75,31 @@ spec =
             If we follow a sequence of operations `ops` to arrive at `result`,
             then undoing those operations in reverse should give us `input`.
             Since some of the operations are destructive, we expect potentially
-            many of them, and as long as all those inputs can be reapplied
-            to get `result`, we are fine.
-
-            TODO: investigate why there are zero-alternative cases.
-            (probably has something to do with the destructive nature?)
+            many of them (or zero, since some operations are destructive
+            and not necessarily have a way of undoing)
+            and as long as all those inputs can be reapplied to get `result`,
+            we are fine.
            -}
-          let inputs = foldM (\cur op -> unapplyOp n op cur) result (reverse ops)
+          let inputs = unperformOps n result ops
               tag = "alternative count: " <> show (length inputs)
           label tag $
             conjoin $ forM_ inputs \inp -> do
-              let applied = foldl' (\cur op -> applyOp n op cur) inp ops
+              let applied = performOps n inp ops
               pure $ applied === result
+      prop "applyOp then unapplyOp is consistent" $ forAll
+        (do
+           n <- chooseInt (5, 10)
+           inp <- genStr n
+           opCount <- chooseInt (1, 10)
+           ops <- replicateM opCount (genOperation n)
+           pure (n, inp, ops))
+        \(n, inp, ops) -> do
+          {-
+            Apply `ops` on `inp` and see whether we can find `inp`
+            after undoing those operations.
+           -}
+          let result = performOps n inp ops
+              unapplied = unperformOps n result ops
+              tag = "alternative count: " <> show (length unapplied)
+          label tag $
+            property (inp `elem` unapplied)
